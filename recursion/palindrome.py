5a1c4845deee741917527d0d62325d0d40b7e134
ivnb <6042430@mail.ru>
2020-11-05 20:57:22 +0000

    normalized

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -2,13 +2,13 @@
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
 word = input()
-def symbolCheck(a, i=0) :
+def is_palindrome(word,i=0) :
   if (i>=len(word)-i-1):
     print ('YES')
   
   elif (word[i]==word[len(word)-i-1]) :
     i=i+1
-    symbolCheck(word, i)
+    is_palindrome(word, i)
   
   else : print ('NO')
-symbolCheck(word)
+is_palindrome(word)
