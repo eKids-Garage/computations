87e6d718139f260e13076c0c89cb355390c2b340
ivnb <6042430@mail.ru>
2020-11-05 20:45:55 +0000

    palindrome

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,5 +1,14 @@
 # Дано слово, состоящее только из строчных латинских букв. 
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
-def is_palindrome(word):
-    return "YES"
+word = input()
+def symbolCheck(a, i=0) :
+  if (i>=len(word)-i-1):
+    print ('YES')
+  
+  elif (word[i]==word[len(word)-i-1]) :
+    i=i+1
+    symbolCheck(word, i)
+  
+  else : print ('NO')
+symbolCheck(word)
