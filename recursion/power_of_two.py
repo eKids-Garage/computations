6b9e894d2215ad7d06ad86b264787979e6f637d5
Retr0 <kirill.nikitin@outlook.com>
2020-11-06 13:38:59 +0000

    recursion/power_of_two.py was done and .replit was changed for doing this task

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -1,5 +1,14 @@
 #Дано натуральное число N. Выведите слово YES, если число N является точной степенью двойки, или слово NO в противном случае.
 #Операцией возведения в степень пользоваться нельзя!
+N = input("Type your number:")
+N = int(N)
 
 def is_power_of_two(N):
+  if N == 1 or N == 2:  
     return 'YES'
+  elif N % 2 != 0:
+    return 'NO'
+  else:
+    return(is_power_of_two(N // 2))
+
+print(is_power_of_two(N))
