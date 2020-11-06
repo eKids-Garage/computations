6e9bfac3caed5eb0403deace38e1b2333d0c01b5
Retr0 <kirill.nikitin@outlook.com>
2020-11-06 15:32:40 +0000

    recursion/sum_of_digits.py was done and .replit was changed to do this task

--- recursion/sum_of_digits.py
+++ recursion/sum_of_digits.py
@@ -1,6 +1,19 @@
 # Дано натуральное число N. Вычислите сумму его цифр.
 # При решении этой задачи нельзя использовать цикл, а также строки, списки, массивы
+N = input("Type your number:")
+N = int(N)
+digit = 0
+Sum = 0
 
 def sum(N):
-    return 0
-
+    
+    global Sum
+    
+    if N >= 1:
+        digit = N % 10
+        Sum = Sum + digit
+        N = N // 10
+        return sum(N)
+    else:
+        return Sum
+print(sum(N))
