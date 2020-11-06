3aa12dfb6b8adbf9caa84464ba5688948d55415b
mkulik05 <mishakulik2@gmail.com>
2020-11-06 16:39:03 +0300

    palindrome

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,5 +1,16 @@
 # Дано слово, состоящее только из строчных латинских букв. 
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
+c=0
 def is_palindrome(word):
+  length=len(word)
+  global c
+  if word[length-c-1]==word[c]:
+    c+=1
+  else:
+    return "NO"
+  if c == length // 2:
     return "YES"
+  return is_palindrome(word)
+
+print(is_palindrome("ankna"))
