65955c5b8c7ad2fb5a233d85b3521d872cb2c02e
AlexeyShelyagin <alexeyshelyagin@gmail.com>
2020-11-06 12:45:41 +0000

    palindrome

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,5 +1,11 @@
 # Дано слово, состоящее только из строчных латинских букв. 
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
-def is_palindrome(word):
-    return "YES"
+def is_palindrome(word, c):
+    if word[len(word) - 1 - c] != word[c]:
+        return "NO"
+    if c < len(word) / 2:
+        return "YES"
+    return is_palindrome(word, c + 1)
+
+print(is_palindrome("abacaba", 0))
