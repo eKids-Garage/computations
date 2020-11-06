211cab548330cb9b78f8d4c5e711e6a0f438e1b4
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2020-11-06 20:23:29 +0300

    Update sum_of_digits.py

--- recursion/sum_of_digits.py
+++ recursion/sum_of_digits.py
@@ -1,6 +1,15 @@
 # Дано натуральное число N. Вычислите сумму его цифр.
 # При решении этой задачи нельзя использовать цикл, а также строки, списки, массивы
 
-def sum(N):
-    return 0
+def sum(N, res):
+    res = res + N % 10
+    N = N // 10
+    if(N != 0):
+      sum(N, res)
+    else:
+      print(res)
+
+N = int(input("Print any number: "))
+res = 0
+sum(N, res)
 
