85cd99bf0701993fbcb94e1f93acf442084fd381
Uladzislau <uladzislau.gmail.com>
2020-11-06 23:32:09 +0300

    palindrome

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -2,4 +2,11 @@
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
 def is_palindrome(word):
-    return "YES"
+    if len(word)==1:
+        return "YES"
+
+    if word[0] == word[-1]:
+        return is_palindrome(word[1:-1])
+
+    return  "NO"
+
