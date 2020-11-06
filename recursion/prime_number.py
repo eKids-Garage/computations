1c7aba5713a8468bee833b1f7ac05943d7537ce3
Adam Sidoryk <adam.sidoryk@gmail.com>
2020-11-06 09:50:20 +0000

    prime number

--- recursion/prime_number.py
+++ recursion/prime_number.py
@@ -1,7 +1,11 @@
 # Дано натуральное число N>1. Проверьте, является ли оно простым. 
 # Программа должна вывести слово YES, если число простое и NO, если число составное. 
-
-def is_prime(N):
+N = int(input("введите число > 1 "))
+c = 2
+def is_prime(N,c):
+  if N % c == 0:
+    return "NO"
+  if c >= N**0.5:
     return "YES"
-
-print("git test")
+  return is_prime(N, c + 1)
+print(is_prime(N,c))  
