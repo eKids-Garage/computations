a4f7e52bf0f50d8f78b89c0995440ff53e6277f6
Retr0 <kirill.nikitin@outlook.com>
2020-11-06 14:55:09 +0000

    recursion/palindrome.py was done and .replit was changed to do this task

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,5 +1,21 @@
 # Дано слово, состоящее только из строчных латинских букв. 
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
-def is_palindrome(word):
-    return "YES"
+word = input("Type your word:")
+Len = len(word)
+c = 0
+
+def is_palindrome(word, c):
+       
+    if word[Len - c - 1] == word[c]:
+        c = c + 1
+    
+    if word [Len - c - 1] != word[c]:
+        return 'NO'
+    
+    if c == Len // 2:
+        return 'YES'
+    
+    return is_palindrome(word, c)    
+    
+print(is_palindrome(word, c))    
