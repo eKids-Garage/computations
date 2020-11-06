11bc4e31d083395fb8fc999cf9993171f264efde
ivnb <6042430@mail.ru>
2020-11-06 10:56:02 +0000

    prime check

--- recursion/prime_number.py
+++ recursion/prime_number.py
@@ -1,5 +1,13 @@
 # Дано натуральное число N>1. Проверьте, является ли оно простым. 
 # Программа должна вывести слово YES, если число простое и NO, если число составное. 
-
-def is_prime(N):
-    return "YES"
+import math
+def is_prime(N,d=2):
+  if (d>math.sqrt(N)):
+    print('prime')
+  elif (N==N//d*d):
+     print('non prime')
+  else :
+    is_prime(N,d=d+1)
+num=input()
+num=int(num)
+is_prime(num)
