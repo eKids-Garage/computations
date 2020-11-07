bd955fa873bf2161ce16a7c8ffd64a2c4e61ab6b
Dzmitry K <dzmitry.kuksau1@gmail.com>
2020-11-07 15:37:17 +0000

    power of two - task done

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -2,4 +2,13 @@
 #Операцией возведения в степень пользоваться нельзя!
 
 def is_power_of_two(N):
-    return 'YES'
+    return divide_by_2(N)
+
+def divide_by_2(N):
+    if (N == 1):
+        return "YES"
+    if (N % 2 != 0):
+        return "NO"
+    return divide_by_2(N/2)
+
+print (is_power_of_two(32768))
