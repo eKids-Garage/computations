57f6dac49d05f77dd1bea762e9afce5dfa42c5ea
Retr0 <kirill.nikitin@outlook.com>
2020-11-07 07:29:51 +0000

    prime_number was done and .replit was changed to do this task

--- recursion/prime_number.py
+++ recursion/prime_number.py
@@ -1,5 +1,29 @@
 # Дано натуральное число N>1. Проверьте, является ли оно простым. 
 # Программа должна вывести слово YES, если число простое и NO, если число составное. 
+N = input("Type your number:")
+N = int(N)
+c = 1
 
 def is_prime(N):
-    return "YES"
+    
+    global c
+    
+    c = c + 1
+    
+    if N == 3:
+        return 'YES' #prime number
+    
+    if c < N:
+        if N % c == 0:
+            return 'NO' #composite number
+    
+    if c == N ** .5:
+        return 'NO' #composite number
+    
+    if c == N // 2:
+        return 'YES' #prime number
+    else:
+        return is_prime(N)
+    
+    
+print(is_prime(N))    
