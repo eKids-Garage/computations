7b20bce7eb7601de0918d803581af356704e8688
Dzmitry K <dzmitry.kuksau1@gmail.com>
2020-11-07 16:46:18 +0000

    sum of digits - task done

--- recursion/sum_of_digits.py
+++ recursion/sum_of_digits.py
@@ -2,5 +2,14 @@
 # При решении этой задачи нельзя использовать цикл, а также строки, списки, массивы
 
 def sum(N):
-    return 0
+    return sum_of_digits(N) 
 
+
+def sum_of_digits(N):
+    if N < 10:
+        return N
+   
+    return N % 10 + sum_of_digits(N // 10)
+
+
+print (sum(9876543210))
