dcc7b165eec8d9018b1b4f293d95d5509f4c214c
IvanSuboch <ivan.suboch9@gmail.com>
2020-11-07 19:55:53 +0000

    I'm done!

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -2,4 +2,19 @@
 #Операцией возведения в степень пользоваться нельзя!
 
 def is_power_of_two(N):
+  if N == 1:
     return 'YES'
+  elif N < 1:
+    return 'NO'
+  else:
+    return is_power_of_two(N / 2)
+
+print(is_power_of_two(1)) # yes
+print(is_power_of_two(2)) # yes
+print(is_power_of_two(4)) # yes
+print(is_power_of_two(16)) # yes
+print(is_power_of_two(1024)) # yes
+print(is_power_of_two(0)) # no
+print(is_power_of_two(3)) # no
+print(is_power_of_two(10)) # no
+print(is_power_of_two(1028)) # no
