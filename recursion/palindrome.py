8966b31165639d3ebbaecd08f9d0078e75d59a17
Nikita Gapankov <gapankovnl@gmail.com>
2020-11-08 18:02:30 +0000

    Solution

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,5 +1,19 @@
-# Дано слово, состоящее только из строчных латинских букв. 
-# Проверьте, является ли это слово палиндромом. Выведите YES или NO.
-
 def is_palindrome(word):
-    return "YES"
+  a = len(word)
+  c = 1
+  b = (a // 2) + 1
+  d = ""
+  if a % 2 == 0:
+    d = "NO"
+  else:
+    while c <= b:
+      if word[c] != word[a - c - 1]:
+        d = "NO"
+        break
+      else:
+        d = "YES"
+      c = c + 1
+  return d
+
+word = str(input())
+print(is_palindrome(word))
