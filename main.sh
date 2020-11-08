5198f8ad122a96de1f252dd1fc53e0b20bcd3017
IvanSuboch <ivan.suboch9@gmail.com>
2020-11-08 12:21:34 +0000

    Is_prime created

---
+++ main.sh
@@ -0,0 +1,1 @@
+python recursion/prime_number.py
--- recursion/prime_number.py
+++ recursion/prime_number.py
@@ -1,5 +1,14 @@
 # Дано натуральное число N>1. Проверьте, является ли оно простым. 
 # Программа должна вывести слово YES, если число простое и NO, если число составное. 
-
+from math import sqrt
 def is_prime(N):
-    return "YES"
+  for X in range(2, int(sqrt(N)) + 1):
+    if N % X == 0:
+      print("NO")
+      return
+  print("YES")
+
+is_prime(9)
+is_prime(7)
+is_prime(37)
+is_prime(37*71)
