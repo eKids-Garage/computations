db0dee7e48edab971e90ff8ffb0563ef6e787a0b
Nikita Gapankov <gapankovnl@gmail.com>
2020-11-08 18:04:02 +0000

    Solution

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -1,5 +1,18 @@
-#Дано натуральное число N. Выведите слово YES, если число N является точной степенью двойки, или слово NO в противном случае.
-#Операцией возведения в степень пользоваться нельзя!
-
 def is_power_of_two(N):
-    return 'YES'
+  c = 0
+  b = 2
+  if N == 1 or N == 2:
+    return "YES"
+  elif N % 2 != 0:
+    return "NO"
+  else:
+    while b <= N / 2:
+      b = b * 2
+      if (b == N / 2):
+        return "YES"
+        break
+      elif (b > N / 2):
+        return "NO"
+        break
+N = int(input())
+print(is_power_of_two(N))
