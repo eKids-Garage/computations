999d47d66eb91741b34454cb125b7c25fdbb5b86
Nikita Gapankov <gapankovnl@gmail.com>
2020-11-08 18:00:08 +0000

    Solution

--- recursion/prime_div.py
+++ recursion/prime_div.py
@@ -1,4 +1,20 @@
-# Дано натуральное число N>1. Выведите все простые множители этого числа k в порядке неубывания с учетом кратности.  
-
-def prime_div(N, k):
-    return k
+N = int(input())
+k = 1
+def is_prime(a):
+  i = 1
+  c = 0
+  if a > 3:
+    while i <= a // 2:
+      if a % i == 0 and i != 1:
+        c = "NO"
+        break;
+      else:
+        c = "YES"
+      i = i + 1
+  else:
+    c = "YES" 
+  return c
+while k < N + 1:
+  if(is_prime(k) == "YES" and N % k == 0):
+    print(k)
+  k = k + 1
