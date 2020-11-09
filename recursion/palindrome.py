c0333b796e0ef68b65bd0f8a18187db6c7f7547a
arnbrz <arinabereza2006@gmail.com>
2020-11-09 15:07:22 +0000

    palindrome

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,5 +1,20 @@
 # Дано слово, состоящее только из строчных латинских букв. 
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
-def is_palindrome(word):
-    return "YES"
+
+word = input()
+
+c=0
+
+
+def is_palindrome(word, c):
+    if c != round(len(word) / 2):
+        if word[c] == word[len(word) - 1 - c]:
+           c += 1
+           is_palindrome(word, c=c)
+        else:
+            print("NO")
+    else:
+        print("YES")
+
+is_palindrome(word, c=c)        
