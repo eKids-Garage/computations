ce2e507b4a07f9f58d6e47b152d747426b2290cc
Михаил Кулик <kulikmihail2@gmail.com>
2020-11-10 18:57:00 +0000

    prime_div

--- recursion/prime_div.py
+++ recursion/prime_div.py
@@ -1,4 +1,13 @@
 # Дано натуральное число N>1. Выведите все простые множители этого числа k в порядке неубывания с учетом кратности.  
 
-def prime_div(N, k):
-    return k
+def prime_div(N, k=2):
+  if N/k<k:
+    print(N)
+  else:
+    if N%k==0:
+      print(k)
+      prime_div(N//k,k)
+    else:
+      prime_div(N,k+1)
+
+prime_div(36)
