417b176ce317a8174668b06985e0ea00eb4f2616
Dmitry Orekhov <dmitry_orekhov@epam.com>
2020-11-14 00:19:30 +0300

    Tail recursion

---
+++ recursion/tail/palindrome.py
@@ -0,0 +1,10 @@
+# Дано слово, состоящее только из строчных латинских букв. 
+# Проверьте, является ли это слово палиндромом. Выведите YES или NO.
+
+
+def is_palindrome(word):
+    return "YES"
+
+
+def is_palindrome_tail(word, n = 0):
+    return "YES"
