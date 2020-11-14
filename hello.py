1296a741871de3778fe4994af191293ca56aa181
Ksu Ksu <xju2005@gmail.com>
2020-11-14 08:51:49 +0000

    word = input() def is_palindrome(word,i=0) :   if (i>=len(word)-i-1):     print ('YES')
      elif (word[i]==word[len(word)-i-1]) :
        i=i+1
        is_palindrome(word, i)
    
      else : print ('NO')
    is_palindrome(word)

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
-# Сделайте ветку по шаблону name-surname-01-hello
-# Добавьте в программу код, который выводит фразу "I'm done!"
-# Запушьте ветку 
+# Дано слово, состоящее только из строчных латинских букв. 
+# Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
+word = input()
+def is_palindrome(word,i=0) :
+  if (i>=len(word)-i-1):
+    print ('YES')
+  
+  elif (word[i]==word[len(word)-i-1]) :
+    i=i+1
+    is_palindrome(word, i)
+  
+  else : print ('NO')
+is_palindrome(word)
