99abe9b016a41ee9736a159dec03ff9585174ddb
Lendrina <elena.suboch5@gmail.com>
2020-11-20 20:07:13 +0000

    Tail Sum of digits done

--- recursion/tail/sum_of_digits.py
+++ recursion/tail/sum_of_digits.py
@@ -3,9 +3,21 @@
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
+N = int(input("Введите число:"))
 def sum(N):
-    return 0
+  if N < 10:
+    return N
+  digit = N % 10
+  return sum(N//10) + digit
 
-def sum_tail(N, s):
-    return 0
+print(sum(N))
+
+def sum_tail(N, s = 0):
+    while N > 0:
+      digit = N % 10
+      s = s + digit
+      return sum_tail(N // 10, s)
+    return s
+
+print(sum_tail(N))
 
