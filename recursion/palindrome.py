6bfff2bd212ae8ad323a0023457eb8323f09546f
rhohloy <rhohloy@gmail.com>
2020-11-20 18:15:03 +0000

    l

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,5 +1,10 @@
 # Дано слово, состоящее только из строчных латинских букв. 
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
-
 def is_palindrome(word):
-    return "YES"
+  s=word[::-1]
+  if word==s:
+    print("Yes")
+  else:
+      print("No")
+
+is_palindrome("топот")
