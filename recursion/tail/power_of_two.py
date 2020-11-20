d85d49647d58e50bd78981a3d90fa8a3bc085a6e
Andrey Mikhailov <AndrianoEngineer@yandex.ru>
2020-11-20 19:49:59 +0000

    1

--- recursion/tail/power_of_two.py
+++ recursion/tail/power_of_two.py
@@ -3,11 +3,38 @@
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
-def is_power_of_two(N):
-    return 'YES'
+def is_power_of_two(n):
+  if n / 2 == 1:
+    print("YES")
+  else:
+    if n % 2 == 0:
+      is_power_of_two(n / 2)
+    else:
+      print("NO")
 
 
-def is_power_of_two_tail(N, n):
-    return 'YES'
 
+def is_power_of_two_tail(n):
+  if n / 2 == 1:
+    print("YES")
+  else:
+    if n % 2 == 0:
+      is_power_of_two_tail(n / 2)
+    else:
+      print("NO")
 
+n = int(input("Number:"))
+k = 2
+is_power_of_two(n)
+is_power_of_two_tail(n)
+
+while n / 2 != 1:
+  if (n / 2) / 2 == 1:
+    print("YES")
+    n = n / 2
+  else:
+    if n % 2 == 0:
+      n = n / 2
+    else:
+      print("NO")
+      n = 2
