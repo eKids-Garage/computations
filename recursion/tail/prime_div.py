127fcb073a8b804fe4bfd05a8f2a1f165dc3cb3b
Andrey Mikhailov <AndrianoEngineer@yandex.ru>
2020-11-20 19:33:04 +0000

    1

--- recursion/tail/prime_div.py
+++ recursion/tail/prime_div.py
@@ -2,9 +2,33 @@
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
-def dividors(N):
+def dividors(n):
     return 0
 
+#не знаю как сделать обычную рекурсию
 
-def dividors_tail(N, k):
-    return 0
+def dividors_tail(n, k):
+  if n / k == 1:
+    print(k)
+  else:
+    if n % k == 0:
+      print(k)
+      dividors_tail(n / k, k)
+    else:
+      dividors_tail(n, k + 1)
+
+n = int(input("Number:"))
+k = 2
+dividors_tail(n, k)
+
+while n / k != 1:
+  if n / (k + 1) == 1:
+    print(k + 1)
+    k = k + 1
+    n = n / k
+  else:
+    if n % k != 0:
+      k = k + 1
+    else:
+      n = n / k
+      print(k)
