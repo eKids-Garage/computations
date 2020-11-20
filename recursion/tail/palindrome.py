5d35927ae743916913d3fc82a408cbe20799d1c3
Andrey Mikhailov <AndrianoEngineer@yandex.ru>
2020-11-20 20:00:35 +0000

    1

--- recursion/tail/palindrome.py
+++ recursion/tail/palindrome.py
@@ -7,5 +7,27 @@ def is_palindrome(word):
     return "YES"
 
 
-def is_palindrome_tail(word, n = 0):
-    return "YES"
+def is_palindrome_tail(word, a):
+  if a != len(word) // 2:
+    if word[a] == word[len(word) - 1 - a]:
+      is_palindrome_tail(word, a + 1)
+    else:
+      print("NO")
+  else:
+    print("YES")
+
+word = input("Word:")
+a = 0
+is_palindrome_tail(word, a)
+
+while a != len(word) // 2:
+  if a + 1 == len(word) // 2:
+    if word[a + 1] == word[len(word) - 1 - a]:
+      print("YES")
+      a = a + 1
+  else:
+    if word[a] == word[len(word) - 1 - a]:
+      a = a + 1
+    else:
+      print("NO")
+      a = len(word) // 2
