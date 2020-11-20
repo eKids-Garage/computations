f9e7ffb90a831709749ebe9afdbe6094e29cde7e
Andrey Mikhailov <AndrianoEngineer@yandex.ru>
2020-11-20 18:58:58 +0000

    1

--- recursion/tail/sum_of_digits.py
+++ recursion/tail/sum_of_digits.py
@@ -3,9 +3,26 @@
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
-def sum(N):
-    return 0
+def sum(n):
+  if n != 0:
+    return n % 10 + sum(n // 10)
+  else:
+    return(n)
 
-def sum_tail(N, s):
-    return 0
+def sum_tail(n, k):
+  if n != 0:
+    k = k + n % 10
+    sum_tail(n // 10, k)
+  else:
+    print(k)    
 
+n = int(input("Number:"))
+k = 0
+sum_tail(n, k)
+print(sum(n))
+
+while n != 0:
+  k = k + n % 10
+  n = n // 10
+
+print(k)
