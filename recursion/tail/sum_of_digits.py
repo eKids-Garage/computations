25c922370921b76fd4660aa626d35772f7726f68
Retr0 <kirill.nikitin@outlook.com>
2020-11-20 16:49:47 +0000

    recursion/tail/sum_of_digits.py was done and .replit was changed to do this task

--- recursion/tail/sum_of_digits.py
+++ recursion/tail/sum_of_digits.py
@@ -2,10 +2,42 @@
 # При решении этой задачи в функциях sum и sum_tail нельзя использовать цикл, а также строки, списки, массивы
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
+N = input("Type your number:")
+N = int(N)
+Sum = 0
+digit = 0
 
+
+#usual recursion(not tail)
 def sum(N):
-    return 0
+  if N <= 1:
+      return N
+  else:
+      return N % 10 + sum(N//10)
+    
+print("\nSolving problem with help of usual recursion(not tail):")    
+print(sum(N))    
 
-def sum_tail(N, s):
-    return 0
 
+#tail recursion
+def sum_tail(N, Sum):
+  if N >= 1:
+    digit = N % 10
+    Sum = Sum + digit
+    N = N // 10
+    return sum_tail(N, Sum)
+  else:
+    return Sum
+      
+print("\nWith help of tail recursion:")
+print(sum_tail(N, Sum))
+
+
+#while loop
+while N >= 1:
+  digit = N % 10
+  Sum = Sum + digit
+  N = N // 10
+  
+print("\nWith help of while loop:")  
+print(Sum)  
