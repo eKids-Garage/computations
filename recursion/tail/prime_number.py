7ed4e8c123a934788092afe8139fa4a94aa1cf5c
ksushaveko <kseniav009@gmail.com>
2020-11-22 20:30:11 +0000

    prime number tail + while solution

--- recursion/tail/prime_number.py
+++ recursion/tail/prime_number.py
@@ -3,9 +3,36 @@
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
-def is_prime(n):
-  return("YES")
+N = int(input())
+def prime(x,c):
+   if c >= x:
+     return "YES"
+   if x%c!=0 and prime(x,c+1) == "YES":
+     return "YES"
+   else:
+     return "NO"
+print(prime(N,2))
 
-    
-def is_prime_tail(n, k):
-  return("YES")
+
+
+def prime_tail(x,c):
+  if x>3 and x % c != 0: 
+    return prime(x,c+1)
+  elif x>3 and x % c == 0 and c < x**(1/2):
+    return "NO"
+  else:
+    return "YES"
+print(prime_tail(N,2)) 
+
+
+
+def prime_while(x,c):
+    if x < 3:
+        return "YES"
+    while c < x**(1/2):
+        if x % c == 0:
+            return "NO"
+        else:
+            c +=1 
+    return "YES"
+print(prime_while(N,2))
