810b818ed1fe05023d7ba8fc4ab6a65dc3bf7a48
Adam Sidoryk <adam.sidoryk@gmail.com>
2020-11-22 14:23:01 +0000

    solution

--- recursion/tail/prime_number.py
+++ recursion/tail/prime_number.py
@@ -2,10 +2,28 @@
 # Программа должна вывести слово YES, если число простое и NO, если число составное. 
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
-
+N = 56677654
 def is_prime(n):
+   #я не смог без дополнительного каунтера
+   return("YES")
+
+def is_prime_tail(n, k):
+  if n % k == 0:
+    return "NO"
+  if k >= n**0.5:
+    return "YES"
+  return is_prime_tail(n, k + 1)
+
+def is_prime_while(n):
+  k = 2
+  while k <= n**0.5:
+    if n % k == 0:
+      return "NO"
+    k += 1
+
   return("YES")
 
-    
-def is_prime_tail(n, k):
-  return("YES")
+print(is_prime_tail(N, 2))  
+
+print(is_prime_while(N))  
+  
