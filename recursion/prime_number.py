9edcf588bc10faf55a5dbe09d89b4870a2a981de
Lisa <74136123+LizKrupets@users.noreply.github.com>
2020-11-22 12:58:20 +0300

    Update prime_number.py

--- recursion/prime_number.py
+++ recursion/prime_number.py
@@ -1,5 +1,34 @@
-# Дано натуральное число N>1. Проверьте, является ли оно простым. 
-# Программа должна вывести слово YES, если число простое и NO, если число составное. 
+def sum(N):
 
-def is_prime(N):
-    return "YES"
+  a=N%10
+  N=N//10  
+
+  if(N==0):
+    return a 
+
+  else:
+    return a+sum(N)
+
+
+
+def sum_tail(N, res):
+  
+  a=N%10
+  N=N//10  
+  res=res+a 
+
+  if(N==0):
+    return res
+
+  else:
+    return sum_tail(N,res)
+
+
+def sum_tail_while(N):
+  res = 0
+  while (N != 0):
+    a = N % 10
+    N=N//10
+    res=res+a  
+    
+  print (res)
