5a8329f7711522632687ef389379e63f6e6f18bf
ksushaveko <kseniav009@gmail.com>
2020-11-23 18:04:15 +0000

    prime-div tail+while solution

--- recursion/tail/prime_div.py
+++ recursion/tail/prime_div.py
@@ -1,10 +1,32 @@
-# Дано натуральное число N>1. Выведите все простые множители этого числа k в порядке неубывания с учетом кратности.  
-# Оформите в виде обычной и хвостовой рекурсии
-# Вариант с хвостовой рекурсией преобразуйте в цикл while
+N = int(input())
 
-def dividors(N):
-    return 0
+def prime_div_tail(n,k):
+    if n < k:
+        return 
+    if n%k == 0:
+        print(k)
+        prime_div_tail(n//k, k)
+    else:
+        prime_div_tail(n, k+1)
+prime_div_tail(N,2)
 
 
-def dividors_tail(N, k):
-    return 0
+def prime_div_while(n,k):
+    while n >= k:
+        if n%k==0:
+            print(k)
+            n=n//k
+        else:
+            k += 1
+prime_div_while(N,2)
+    
+
+def prime_div(n,k):
+   if n/k < k:
+     return [n]
+   else:
+     if n%k == 0:
+       return [k]+prime_div(n//k,k)
+     else:
+       return prime_div(n,k+1)
+print(prime_div(N,2))
