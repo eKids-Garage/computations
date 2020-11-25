c19d76330095287567f32c787a1b6f99bc7b8c19
Uladzislau Zablotski <73661099+uladzislauzablotski@users.noreply.github.com>
2020-11-25 14:12:51 +0300

    Update power_of_two.py

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -1,9 +1,9 @@
 
 
 def is_power_of_two(N):
-    if N==0 or N==1:
+    if N == 1:
         return "YES!"
-    elif N%2!=0:
+    elif N%2 != 0 or N == 0:
         return "NO!"
 
     return is_power_of_two(N/2)
