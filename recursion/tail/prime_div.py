fabb61e10cf8e8a21a0a727faa99b5ed9dd5a1eb
Retr0 <kirill.nikitin@outlook.com>
2020-11-27 19:12:17 +0000

    recursion/tail/prime_div.py was done and .replit was changed to do this task

--- recursion/tail/prime_div.py
+++ recursion/tail/prime_div.py
@@ -2,9 +2,34 @@
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
-def dividors(N):
-    return 0
+N = input("Type your number:")
+N = int(N)
+k = 2
+
+#Не смог решить задачу при помощи обычной рекурсии, так как не понимаю, как можно решить эту задачу без передачи второго аргумента
 
 
 def dividors_tail(N, k):
-    return 0
+    if N * k != k:
+        if N % k == 0:
+            print(k)
+            N = N // k
+            dividors_tail(N, k)
+        else:
+            k = k + 1
+            dividors_tail(N, k)
+
+print("Solving problem with help of tail recursion:")
+dividors_tail(N, k)
+
+
+print("\nSolving problem with help of while loop:")      
+
+while N * k != k:
+  if N % k == 0:
+      print(str(k))
+      N = N // k
+  else:
+      k = k + 1
+
+     
