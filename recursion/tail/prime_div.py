9e89b4c2b77237a85f37b956a1f38262727a5113
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2020-11-28 19:00:52 +0300

    Update prime_div.py

--- recursion/tail/prime_div.py
+++ recursion/tail/prime_div.py
@@ -2,9 +2,33 @@
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
+#извините, но решить задачу обычной рекурсией у меня не получилось
 def dividors(N):
     return 0
 
-
 def dividors_tail(N, k):
-    return 0
+    if(N != 1):
+      if (N % k == 0):
+        N = N / k
+        print(k)
+        k = 2
+        dividors_tail(N, k)
+      else:
+        k = k + 1
+        dividors_tail(N, k)
+
+def dividors_while(N, k):
+    while N > 1:
+      if (N % k == 0):
+        N = N / k
+        print(k)
+        k = 2
+      else:
+        k = k + 1
+
+N = int(input("Input any number: "))
+k = 2
+print("Tail recursion: ")
+dividors_tail(N, k)
+print("While: ")
+dividors_while(N, k)
