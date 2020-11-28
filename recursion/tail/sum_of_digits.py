d766f700adf221b42bde37794abc501400745927
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2020-11-28 17:09:30 +0300

    Update sum_of_digits.py

--- recursion/tail/sum_of_digits.py
+++ recursion/tail/sum_of_digits.py
@@ -2,10 +2,32 @@
 # При решении этой задачи в функциях sum и sum_tail нельзя использовать цикл, а также строки, списки, массивы
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
-
 def sum(N):
-    return 0
+  res = N % 10
+  N = N // 10
+  if N == 0:
+    return res
+  else:
+    return res + sum(N)
 
-def sum_tail(N, s):
-    return 0
+def sum_tail(N, res):
+  res = res + N % 10 
+  N = N // 10
+  if N == 0:
+    return res
+  else: 
+    return sum_tail(N, res)
+
+def sum_while(N):
+  res = 0
+  while N != 0:
+    res = res + N % 10
+    N = N // 10
+  return res
+
+N = int(input("Input any number: "))
+res = 0
+print(sum(N))
+print(sum_tail(N, res)) 
+print(sum_while(N))
 
