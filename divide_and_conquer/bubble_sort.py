02a00f47d88369baa53c7b17be410783a07b74e6
IvanSuboch <ivan.suboch9@gmail.com>
2020-12-03 19:16:08 +0000

    I'm done!

--- divide_and_conquer/bubble_sort.py
+++ divide_and_conquer/bubble_sort.py
@@ -2,5 +2,16 @@
 # как итеративно (используя цикл), так и рекурсивно.
 
 def sort_bubble (arr):
-    return arr
+  for j in range(len(arr) - 1, 0, -1):
+    for i in range(0, j):
+      if arr[i] > arr[i + 1]:                                 
+        temp = arr[i + 1]
+        arr[i + 1] = arr[i]
+        arr[i] = temp
+  return arr
+
+print(sort_bubble([1]))
+print(sort_bubble([2, 1]))
+print(sort_bubble([3, 2, 1]))
+print(sort_bubble([5, 4, 3, 2, 1]))
 
