9ba308b4762149e7844bd7c6e8e1154ab5abdd81
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2020-12-03 12:35:27 +0300

    Update bubble_sort.py

--- divide_and_conquer/bubble_sort.py
+++ divide_and_conquer/bubble_sort.py
@@ -1,6 +1,48 @@
 # Реализуйте алгоритм сортировки пузырьком. Можете попробовать реализовать его
 # как итеративно (используя цикл), так и рекурсивно.
 
-def sort_bubble (arr):
-    return arr
+#sorting, using while
+def arraySortingWhile(array):
+  i = 0
+  n = 0
+  while n < len(array):
+    i = 0
+    while i < len(array) - 1: 
+      if array[i] > array[i + 1]:
+        num = array[i + 1]
+        array[i + 1] = array[i]
+        array[i] = num
+      i = i + 1
+    n = n + 1
+  return array
+
+#К сожалению сортировку с помощью рекурсии в одну функцию у меня поместить не получилось
+#sorting, using recurcion
+def arraySortingRecursionFirstStep(array, n):
+  i = 0
+  if n < len(array):
+    arraySortingRecursionSecondStep(array, i)
+    n = n + 1
+    arraySortingRecursionFirstStep(array, n)
+  else:
+    print(array)
+def arraySortingRecursionSecondStep(array, i):
+  if i < len(array) - 1:
+    if array[i] > array[i + 1]:
+          num = array[i + 1]
+          array[i + 1] = array[i]
+          array[i] = num
+    i = i + 1
+    arraySortingRecursionSecondStep(array, i)
+
+
+
+array = [0, 5, 4, 8, 6, 1, 3, 2, 9, 7]
+arrayTwo = [0, 5, 4, 8, 6, 1, 3, 2, 9, 7]
+n = 0
+print("Sorting, using while:")
+print(arraySortingWhile(array))
+print("Sorting, using recursion:")
+arraySortingRecursionFirstStep(arrayTwo, n)
+
 
