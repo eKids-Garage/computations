3f8c2ba4f1cb05b71c139941836b335401e9bd8a
Adam Sidoryk <adam.sidoryk@gmail.com>
2020-12-03 17:55:01 +0000

    solution

--- divide_and_conquer/bubble_sort.py
+++ divide_and_conquer/bubble_sort.py
@@ -1,19 +1,16 @@
 # Реализуйте алгоритм сортировки пузырьком. Можете попробовать реализовать его
 # как итеративно (используя цикл), так и рекурсивно.
-#def sort_bubble(arr):
 
+def sort_bubble(arr):
+  count = len(arr) - 1
+  while count != 0:
+    j = 0 
+    while j < count:
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+      j += 1  
+    count -= 1
 
+  print(arr)      
 
-
-lst = [5,8,0,2,6,10,4,9,7,1,3]
-count = len(lst) - 1
-while count != 0:
-  j = 0 
-  while j < count:
-    if lst[j] > lst[j + 1]:
-      lst[j], lst[j + 1] = lst[j + 1], lst[j]
-    j += 1  
-  count -= 1
-
-print(lst)      
-
+sort_bubble([5,8,0,2,6,10,4,9,7,1,3])
