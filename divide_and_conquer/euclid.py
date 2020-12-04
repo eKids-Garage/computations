9dd1057ff209b9c64f085aadd9d1410f9ff149fa
Kirill Tsybenka <kirill.tsybenka@gmail.com>
2020-12-04 21:51:18 +0000

    euclid

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,15 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
+a = int(input ("1 сторона: "))
+b = int(input ("2 сторона: "))
+
 def euclidus(a, b):
-    return a
+  if b > a :
+    a , b = b , a
+  if a%b == 0:
+    return b
+  else:
+    return euclidus(a - ((a//b)*b), b)
+
+print("максимальный квадрат: ", euclidus(a,b))
+
