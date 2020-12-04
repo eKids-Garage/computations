d8d7c3cd12669faec3f76f3ce5d90e685a8c019b
Adam Sidoryk <adam.sidoryk@gmail.com>
2020-12-04 16:36:51 +0000

    solution

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,13 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
 def euclidus(a, b):
-    return a
+  if a != b:
+    if a > b:
+      a -= b
+    else:
+      b -= a
+  else:
+    print(b)  
+    quit()    
+  euclidus(a,b)    
+euclidus(400,600)   
