a6fcf5bf3d62f430784ed845648edff38f25fb46
DimaRedlin <dimaredlin715@gmail.com>
2020-12-04 13:09:34 +0000

    mmm NICE

--- divide_and_conquer/bubble_sort.py
+++ divide_and_conquer/bubble_sort.py
@@ -1,6 +1,23 @@
 # Реализуйте алгоритм сортировки пузырьком. Можете попробовать реализовать его
 # как итеративно (используя цикл), так и рекурсивно.
 
-def sort_bubble (arr):
-    return arr
+from random import randint
+N = int(input('введите длинну массива ')) #длинна массива;) незнаю зачем но я её сделял
+O = int(input('введите максимальное число которое может быть в массиве ')) #незнаю зачем но я это сделял
+a = []
+
+def bubble(array):
+  for i in range(N-1):
+    for j in range(N-i-1):
+      if array[j] > array[j+1]:
+        K = array[j]
+        array[j] = array[j+1]
+        array[j+1] = K
+
+for i in range(N):
+  a.append(randint(1, O))
+
+print(a)
+bubble(a)
+print(a)
 
