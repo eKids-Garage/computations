29cb0c377f34e3474367f672b92ab9d5767d843f
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2020-12-04 15:27:49 +0300

    Update euclid.py

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,26 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
-def euclidus(a, b):
-    return a
+def euclidusWhile(m, n):
+  while m != n:
+    if(m > n):
+      m = m - n
+    if(n > m):
+      n = n - m
+  return m
+
+def euclidus(m, n):
+  if (m > n):
+    m = m - n
+    return euclidus(m, n)
+  if (m < n):
+    n = n - m
+    return euclidus(m, n)
+  if (m == n):
+    return m
+
+m = int(input("Input m: "))
+n = int(input("Input n: "))
+print("Greatest Common Factor (using Recursion) is: ")
+print(euclidus(m, n))
+print("Greatest Common Factor (using While) is: ")
+print(euclidusWhile(m, n))
