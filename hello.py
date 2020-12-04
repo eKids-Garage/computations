2e79bca094c6a5b7c2dddc6f9802e3fe791dee4d
Ksu Ksu <xju2005@gmail.com>
2020-12-04 17:33:02 +0000

    ht

--- hello.py
+++ hello.py
@@ -1,15 +1,18 @@
 from random import randint
 
+def bubble(array):
+    for i in range(N-1):
+        for j in range(N-i-1):
+            if array[j] > array[j+1]:
+                buff = array[j]
+                array[j] = array[j+1]
+                array[j+1] = buff
+
 N = 10
 a = []
 for i in range(N):
     a.append(randint(1, 99))
+
 print(a)
-
-
-for i in range(N-1):
-    for j in range(N-i-1):
-        if a[j] > a[j+1]:
-            a[j], a[j+1] = a[j+1], a[j]
-
+bubble(a)
 print(a)
