703872945e1154ce1985a6b30bc32d407158f1b6
Ksu Ksu <xju2005@gmail.com>
2020-12-04 17:29:22 +0000

    ht

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
-# Сделайте ветку по шаблону name-surname-01-hello
-# Добавьте в программу код, который выводит фразу "I'm done!"
-# Запушьте ветку 
+from random import randint
 
+N = 10
+a = []
+for i in range(N):
+    a.append(randint(1, 99))
+print(a)
+
+
+for i in range(N-1):
+    for j in range(N-i-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+
+print(a)
