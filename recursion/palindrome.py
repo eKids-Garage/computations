80f58846bb03c1ae1bb32609ec8b10603ebef29a
Rostislav <rostislavbudarin@gmail.com>
2020-12-04 21:00:50 +0000

    123

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,5 +1,15 @@
 # Дано слово, состоящее только из строчных латинских букв. 
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
-def is_palindrome(word):
-    return "YES"
+s=str(input())
+l=len(s)
+k=0
+def is_palindrome(s, k):   
+    if s[l-k-1] == s[k]:
+        k = k + 1
+    if s[l-k-1] != s[k]:
+        return 'NO'
+    if k == l//2:
+        return 'YES'
+    return is_palindrome(s, k)    
+print(is_palindrome(s, k))
