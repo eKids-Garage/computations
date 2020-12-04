73a4915fecb4f063a310c3103a26110a9d355d6b
Dzmitry K <dzmitry.kuksau1@gmail.com>
2020-12-04 16:50:04 +0000

    euclid - task done

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,9 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
 def euclidus(a, b):
-    return a
+    r = a % b
+    if r == 0:
+        return b
+    return euclidus(b, r)
+
+print(euclidus(90, 60))
