41044e2f9f1a0ade59672791531a018e302e4a5b
max <mrom06@ya.ru>
2020-12-04 19:43:20 +0300

    Ok it just another task

--- divide_and_conquer/bubble_sort.py
+++ divide_and_conquer/bubble_sort.py
@@ -2,5 +2,17 @@
 # как итеративно (используя цикл), так и рекурсивно.
 
 def sort_bubble (arr):
+    pos = 0
+    unsorted = len(arr) - 1
+    while unsorted > 0:
+        while pos < unsorted:
+            if arr[pos+1] < arr[pos]:
+                t = arr[pos]
+                arr[pos] = arr[pos+1]
+                arr[pos+1] = t
+            pos += 1
+        pos = 0
+        unsorted -= 1
     return arr
 
+print(sort_bubble([1,5,8,4,1,2]))
--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,11 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
 def euclidus(a, b):
-    return a
+    if max(a,b) % min(a,b) == 0:
+        return min(a,b)
+    return euclidus(min(a,b), max(a,b) % min(a,b))
+
+print(euclidus(6*4, 6))
+print(euclidus(6*4, 6*2))
+print(euclidus(6*4, 6*3))
+print(euclidus(2*5, 3*7))
