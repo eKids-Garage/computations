5bde673d5f19f1bf2342f555a36bca1aaa51edf3
Retr0 <kirill.nikitin@outlook.com>
2020-12-04 16:43:00 +0000

    divide_and_conquer/euclid.py was done and .replit was changed to do this task

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,24 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
+
+#data retrieval
+side_1 = input("Type the 1st side of your rectangle:")
+side_1 = int(side_1)
+
+side_2 = input("Type the second side of your rectangle:")
+side_2 = int(side_2)
+
+
+#main function
 def euclidus(a, b):
-    return a
+    if a == b:
+        return a
+    if a > b:
+        return euclidus(a-b, b)
+    if a < b:
+        return euclidus(a, b-a)
+
+
+#response output
+print( 'You will get square ' + str(euclidus(side_1, side_2)) + '×' +
+       str(euclidus(side_1, side_2)))    
