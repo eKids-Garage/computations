637a51785958063fede839982d253e5ab0351a8a
Maria Zatsepina <zatsepina.maria@gmail.com>
2020-12-05 06:57:40 +0000

    homework

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,17 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
 def euclidus(a, b):
-    return a
+  if a != b and  a%b!= 0:
+    c = b
+    b = a%b
+    a = c
+    euclidus(a, b)
+    return b
+    
+  else:
+    return min(a, b)
+  
+
+    
+
+print(euclidus(8, 7))
