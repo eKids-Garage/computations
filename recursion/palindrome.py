ed87bdaaf020905b5897a4c20915eecd8abee836
fedor <fedor.tryfanau@gmail.com>
2020-12-05 08:12:20 +0000

    task

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -2,4 +2,13 @@
 # Проверьте, является ли это слово палиндромом. Выведите YES или NO.
 
 def is_palindrome(word):
-    return "YES"
+	if len(word)<2:
+		return"YES"
+
+	elif word[0]==word[-1]:
+		return is_palindrome(word[1:-1])
+	else:
+		return"NO"
+
+
+	
