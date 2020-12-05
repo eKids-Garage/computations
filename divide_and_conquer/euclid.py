633c20d431be87879238b0b661e389631f103e5b
Yulia Kanoplich <yulia.kanoplich@gmail.com>
2020-12-05 09:18:41 +0000

    homework

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,14 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
+A = int(input())
+B = int(input())
+
 def euclidus(a, b):
-    return a
+  while a != 0 and b != 0:
+    if a > b:
+      a = a % b
+    else:
+      b = b % a
+  print(a + b)
+
+euclidus(A,B)
