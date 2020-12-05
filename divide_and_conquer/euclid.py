95e0ba4003f9699b0ffc8f7373f2e64464eaebdc
Andrey Mikhailov <AndrianoEngineer@yandex.ru>
2020-12-05 08:19:04 +0000

    1

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,14 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
-def euclidus(a, b):
-    return a
+def gcd(n, m):
+  if n == m:
+    return(n)
+  else:
+    if n > m:
+      return gcd(n - m, m)
+    else:
+      return gcd(n, m - n)
+
+n = int(input("Number:"))
+m = int(input("Number:"))
+print(gcd(n, m))
