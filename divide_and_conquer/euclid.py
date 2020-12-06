0c14aad80529a0ab6b9efda3737c14732dd3b46c
Lendrina <elena.suboch5@gmail.com>
2020-12-06 17:04:25 +0000

    Euclid done

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,13 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
-
+a = int(input("Введите число:"))
+b = int(input("Введите число:"))
 def euclidus(a, b):
+  if a == b:
     return a
+  elif a > b:
+    a = a - b
+  elif a < b:
+    b = b - a
+  return euclidus(a, b)
+print('Квадрат со тороной ', euclidus(a, b))
+
