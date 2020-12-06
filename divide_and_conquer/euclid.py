4f1a74d9e97b98436642229bbc69d0868d840af3
rhohloy <rhohloy@gmail.com>
2020-12-06 17:05:34 +0000

    l

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,18 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
-def euclidus(a, b):
-    return a
+def euclidus(a, b): 
+ while a!=b:
+   if a>b:
+     a=a-b
+   else:
+     b=b-a 
+ return a       
+   
+          
+
+
+
+
+         
+
+print(euclidus(840,320))
