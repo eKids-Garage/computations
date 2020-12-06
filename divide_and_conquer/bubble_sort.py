ada08a47b59cd92f273019c93067f5ee349471fe
Lendrina <elena.suboch5@gmail.com>
2020-12-06 19:10:52 +0000

    Bubble done

--- divide_and_conquer/bubble_sort.py
+++ divide_and_conquer/bubble_sort.py
@@ -1,6 +1,20 @@
 # Реализуйте алгоритм сортировки пузырьком. Можете попробовать реализовать его
 # как итеративно (используя цикл), так и рекурсивно.
+arr = [567, 567890, 234, 6, 5]
+def sort_bubble(arr):
+  for j in range(1, len(arr)):
+    for i in range(0, len(arr) - j):
+      if arr[i] > arr[i+1]:
+        temp = arr[i+1]
+        arr[i+1] = arr[i]
+        arr[i] = temp
+  return arr
 
-def sort_bubble (arr):
-    return arr
+print(sort_bubble(arr))
+
+  
+
+
+
+
 
