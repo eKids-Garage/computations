c80307e08b12c5c08b6523609a5c2389294294ac
Dmitry Orekhov <dmitry_orekhov@epam.com>
2020-12-09 14:36:31 +0300

    Merge sort - solution

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -4,8 +4,45 @@
 # реализовать оба варианта
 
 
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    for k in range(0, len(left) + len(right)):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+            if i == len(left):
+                result.extend(right[j:])
+                break
+        else:
+            result.append(right[j])
+            j += 1
+            if j == len(right):
+                result.extend(left[i:])
+                break
+    
+    return result
+
+    
+
 def merge_sort(arr):
+    n = len(arr)
+    if n == 1:
+        return arr
+
+    left = arr[:n//2]
+    right = arr[n//2:n]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+    arr = merge(left, right)
+
     return arr
 
 def merge_sort_tail(arr):
     return arr
+
+
+print(merge_sort([6, 1, 5, 3]))
+
