9c2d253d730f3770217ad4a1614d424c5a97a597
Nikolai Kravchenko <nikolai.i.kravchenko@gmail.com>
2020-12-12 06:36:08 +0000

    Implemented euclid and euclid_while functions.

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,19 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
-def euclidus(a, b):
-    return a
+def euclid(m, n):
+    if m == n:
+        print(n)
+    elif n > m:
+        n -= m
+        euclid(m, n)
+    elif m > n:
+        m -= n
+        euclid(m, n)
+
+def euclid_while(m, n):
+    while m != n:
+        if n > m:
+            n -= m
+        elif m > n:
+            m -= n
+    print(n)
