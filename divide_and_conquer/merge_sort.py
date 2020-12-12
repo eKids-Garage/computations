dc048bb8e6ab9acb9b2b398d13e2df5e73f1bfee
Nikolai Kravchenko <nikolai.i.kravchenko@gmail.com>
2020-12-12 16:58:36 +0000

    Implemented merge and merge_sort functions.

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -3,9 +3,35 @@
 # так и при помощи хвостовой рекурсии. Желательно, но необязательно
 # реализовать оба варианта
 
-def merge_sort(arr):
-    return arr
+def merge(a, b):
+    c = []
+    i = 0
+    j = 0
+    while len(c) != len(a) + len(b):
+        if a[i] < b[j]:
+            c.append(a[i])
+            i += 1
+        else:
+            c.append(b[j])
+            j += 1
+        if i == len(a):
+            while j < len(b):
+                c.append(b[j])
+                j += 1
+        if j == len(b):
+            while i < len(a):
+                c.append(a[i])
+                i += 1
+            
+    return c
+    
 
-def merge_sort_tail(arr):
-    return arr
+def merge_sort(a):
+    if len(a) == 1:
+        return a
+    else:
+        n = len(a) // 2
+        a1 = merge_sort(a[:n])
+        a2 = merge_sort(a[n:])
+        return merge(a1, a2)
 
