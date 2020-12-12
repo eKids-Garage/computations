fca8d1337586f4a1174cf41bd4963ca2dc65f6ee
Nikolai Kravchenko <nikolai.i.kravchenko@gmail.com>
2020-12-12 06:54:25 +0000

    Implemented power_of_two function.

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -1,5 +1,15 @@
 #Дано натуральное число N. Выведите слово YES, если число N является точной степенью двойки, или слово NO в противном случае.
 #Операцией возведения в степень пользоваться нельзя!
 
-def is_power_of_two(N):
-    return 'YES'
+def power_of_two(n):
+    if n < 3:
+        if n == 2:
+            print ("YES")
+        else:
+            print("NO")
+    else:
+        n /= 2
+        power_of_two(n)
+
+n = int(input())
+power_of_two(n)
