67e04d288741dc86f9fde57fd0f507c0d442309b
max <mrom06@ya.ru>
2020-12-12 19:08:29 +0300

    Ono samo

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -1,11 +1,44 @@
 # Реализуйте сортировку слиянием (merge sort)
-# Попытайтесь реализовать ее как при помощи обычной, 
+# Попытайтесь реализовать ее как при помощи обычной,
 # так и при помощи хвостовой рекурсии. Желательно, но необязательно
 # реализовать оба варианта
 
+import random
+
 def merge_sort(arr):
-    return arr
+    # print(arr)
+    if len(arr) <= 1:
+        return arr
+    elif len(arr) == 2:
+        if arr[0] < arr[1]:
+            return arr
+        else:
+            return arr[::-1]
+
+    oe = arr[len(arr) // 2]
+    L = []
+    R = []
+    for el in arr[:len(arr) // 2]:
+        if el <= oe:
+            L.append(el)
+        else:
+            R.append(el)
+
+    for el in arr[(len(arr) // 2) + 1:]:
+        if el <= oe:
+            L.append(el)
+        else:
+            R.append(el)
+
+    return merge_sort(L) + [oe] + merge_sort(R)
 
 def merge_sort_tail(arr):
     return arr
 
+def t(arr):
+    random.shuffle(arr)
+    return arr
+
+print(merge_sort(t([1,2,5])))
+print(merge_sort(t([1,2,5,4,8,7,9])))
+print(merge_sort(t([1,2,5,6,6,6,4,5])))
--- recursion/karatsuba.py
+++ recursion/karatsuba.py
@@ -2,14 +2,30 @@ def rec_mult(x, y, n):
     if n == 1:
         print("n = 1: x = {0}, y = {1}".format(x, y))
         return x*y
-    else: 
+    else:
         a = x // 10**(n//2)
         b = x % 10**(n//2)
         c = y // 10**(n//2)
         d = y % 10**(n//2)
 
         print("n = {4}: a = {0}, b = {1}, c = {2}, d = {3}".format(a, b, c, d, n))
+        ac = rec_mult(a, c, n//2)
+        bd = rec_mult(b, d, n//2)
+        abcd = rec_mult(a+b, c+d, n//2)
+        return 10**n * ac + 10**(n//2) * (abcd - ac - bd) + bd
 
-        return 10**n * rec_mult(a, c, n//2) + 10**(n//2) * (rec_mult(a, d, n//2) + rec_mult(b, c, n//2)) + rec_mult(b, d, n//2)
+def getLen(a):
+    an = 1
+    ann = 1
+    while a >= ann:
+        an += 1
+        ann *= 10
+    return an - 1
 
-print(rec_mult(9656, 8578, 4))
+
+def karatsuba(a, b):
+    an = getLen(a)
+    bn = getLen(b)
+    return rec_mult(a,b,max(an, bn))
+
+print(karatsuba(121, 7788)) # 7788 - позвони и мы подбросим (оно само)
