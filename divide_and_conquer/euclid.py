d11cb0e31fe7a3849eff9d15b621a5063864bc2e
Nikita Gapankov <gapankovnl@gmail.com>
2020-12-13 12:10:21 +0000

    Solution

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,18 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
 def euclidus(a, b):
-    return a
+  if(a != 0 and b != 0):
+    if (a != b):
+      while(a != b):
+          if a > b:
+              a = a - b
+          else:
+            b = b - a
+      return(a)
+    else:
+      return(a)
+  else:
+    return(a + b)
+a = int(input())
+b = int(input())
+print(euclidus(a, b))
