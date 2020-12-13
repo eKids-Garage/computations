b0f68163e9fb5607490899f578b851355da7f98b
Lisa <74136123+LizKrupets@users.noreply.github.com>
2020-12-13 14:19:44 +0300

    Update power_of_two.py

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -1,10 +1,10 @@
-def func(N):
-  if (N == 2):
+def is_power_of_two(N):
+  if (N == 2, N == 1):
     print ("YES")
     return
   if (N % 2 == 0):
     N = N // 2
-    func(N)
+    is_power_of_two(N)
 
   elif (N % 2 != 0):
     print ("NO")
