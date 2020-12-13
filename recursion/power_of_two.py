37eb7afcf65b0a8ebde9b35388cef08fd3f77a34
Nikita Gapankov <gapankovnl@gmail.com>
2020-12-13 12:30:34 +0000

    Solution

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -1,18 +1,10 @@
 def is_power_of_two(N):
-  c = 0
-  b = 2
-  if N == 1 or N == 2:
-    return "YES"
-  elif N % 2 != 0:
-    return "NO"
-  else:
-    while b <= N / 2:
-      b = b * 2
-      if (b == N / 2):
-        return "YES"
-        break
-      elif (b > N / 2):
-        return "NO"
-        break
+  if(N == 1):
+    return 'YES'
+  if(N % 2 != 0):
+    return 'NO'
+  if(N == 2):
+    return 'YES'
+  return(is_power_of_two(N // 2))
 N = int(input())
 print(is_power_of_two(N))
