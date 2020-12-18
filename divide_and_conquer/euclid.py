2ff95c55882fde07a7b157e4e750f9a485bff7f1
ivnb <6042430@mail.ru>
2020-12-18 19:16:39 +0000

    never too late)

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,14 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
 def euclidus(a, b):
-    return a
+  if a%b==0:
+    print(b)
+  else:
+    if a>b:
+     euclidus(b, a%b)
+    else:
+     euclidus(a, b%a)
+
+euclidus(9823461, 2345673345)
+
+    
