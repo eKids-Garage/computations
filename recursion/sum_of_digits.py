ba64ae98a68b6514343a7bfa8c8229634d4ddec2
Yulia Kanoplich <yulia.kanoplich@gmail.com>
2020-12-18 19:57:52 +0000

    homework

--- recursion/sum_of_digits.py
+++ recursion/sum_of_digits.py
@@ -1,6 +1,16 @@
 # Дано натуральное число N. Вычислите сумму его цифр.
 # При решении этой задачи нельзя использовать цикл, а также строки, списки, массивы
+n=int(input())
 
 def sum(N):
-    return 0
+  summa = 0
+  digit = 0
+  while N>0:
+    digit = N%10
+    summa = summa+digit
+    N = N//10
+  print(summa)
+    
 
+
+sum(n)
