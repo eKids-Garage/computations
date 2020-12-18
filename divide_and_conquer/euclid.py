227e779db6ef6a5fdcc90791ff8ffe5093ad25c3
valentinhilko13 <garageepam12367@gmail.com>
2020-12-18 10:11:32 +0000

    .

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -15,7 +15,7 @@ print(euclid(252, 441))     #63
 print(euclid(441, 1080))    #9       
 print(euclid(1080,720))     #360 
 print(euclid(360,480))     # 120
-print(euclid(7*8,8))        #8
+print(euclid(7*8+8,8))        #8
 """
 a= int(input("Введите 1 число:"))
 b = int(input("Введите 2 число:"))
