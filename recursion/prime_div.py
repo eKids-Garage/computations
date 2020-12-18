de87440255fa73fa6cde6505f064f2c536baf9fe
DimaRedlin <dimaredlin715@gmail.com>
2020-12-18 18:49:00 +0000

    ///

--- recursion/prime_div.py
+++ recursion/prime_div.py
@@ -1,9 +1,13 @@
 # Дано натуральное число N>1. Выведите все простые множители этого числа k в порядке неубывания с учетом кратности.  
 
 def divide(N, k):
-<<<<<<< HEAD
-    return k
-=======
-    return k
-  
->>>>>>> 4f07df098211a96d113141a9479aa3dc8f991dba
+  if N < k:
+    return
+  while N % k < k:
+    if N % k == 0:
+      print(k)
+      return divide(N // k, k)
+    else:
+      return divide (N, k + 1)
+
+divide(8, 2)
