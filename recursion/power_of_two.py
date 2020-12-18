fb3f1505cc1c2ed61730afa34c5055cbc9c43658
Alexey Zhuravel <zhuravel.lesha2005@gmail.com>
2020-12-18 19:48:01 +0000

    .

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -2,4 +2,14 @@
 #Операцией возведения в степень пользоваться нельзя!
 
 def is_power_of_two(N):
-    return 'YES'
+  if N == 2 or N == 1:
+    print("YES")
+  else:
+    if N % 2 == 0:
+      N = N // 2
+      is_power_of_two(N)
+    else:
+      print("NO")
+
+
+is_power_of_two(3)
