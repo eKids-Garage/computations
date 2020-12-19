321a9df43de864ea8ba70335ce1ab08a073a1def
ksushaveko <kseniav009@gmail.com>
2020-12-19 20:40:37 +0000

    merge sort solution

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -1,11 +1,34 @@
-# Реализуйте сортировку слиянием (merge sort)
-# Попытайтесь реализовать ее как при помощи обычной, 
-# так и при помощи хвостовой рекурсии. Желательно, но необязательно
-# реализовать оба варианта
+from random import randint
+N = randint(2,10)
+arr = [randint(-10,10) for i in range(N)]
+print(arr)
 
 def merge_sort(arr):
-    return arr
+  if len(arr) < 2:
+    return(arr)
+  else:
+    L = merge_sort(arr[:len(arr)//2])
+    R = merge_sort(arr[len(arr)//2:])
+    return mergeLR(L, R)
 
-def merge_sort_tail(arr):
-    return arr
 
+def mergeLR(L, R):
+  result = []
+  i = 0
+  j = 0
+  while i < len(L) and j < len(R):
+    if L[i] < R[j]:
+      result.append(L[i])
+      i += 1
+    else:
+      result.append(R[j])
+      j += 1
+  while i < len(L):
+    result.append(L[i])
+    i+=1
+  while j < len(R):
+    result.append(R[j])
+    j+=1
+  return result
+  
+print(merge_sort(arr))
