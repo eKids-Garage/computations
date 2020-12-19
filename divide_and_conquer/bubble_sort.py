4846841f9b21356bda89ed4610a4ebf635a8102f
Nikita Gapankov <gapankovnl@gmail.com>
2020-12-19 15:51:01 +0000

    solution

--- divide_and_conquer/bubble_sort.py
+++ divide_and_conquer/bubble_sort.py
@@ -2,5 +2,20 @@
 # как итеративно (используя цикл), так и рекурсивно.
 
 def sort_bubble (arr):
-    return arr
-
+  l = len(arr)
+  a = 0
+  b = 0
+  for i in range(l - 1):
+    for j in range(l - i - 1):
+      if arr[j + 1] < arr[j]:
+        a = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = a
+  return arr
+length = int(input("Введите длину массива "))
+arr = [0] * length
+for k in range(length):
+  arr[k] = int(input())
+sort_bubble(arr)
+for k in range(length):
+  print(arr[k])
