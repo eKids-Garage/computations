d49d613167f627d28fb8a841f43a7cd5a6327e5c
ivnb <6042430@mail.ru>
2020-12-19 13:02:46 +0000

    sorry for being late

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -1,11 +1,32 @@
-# Реализуйте сортировку слиянием (merge sort)
-# Попытайтесь реализовать ее как при помощи обычной, 
-# так и при помощи хвостовой рекурсии. Желательно, но необязательно
-# реализовать оба варианта
-
 def merge_sort(arr):
-    return arr
+  i=0
+  j=0
+  l=len(arr)
+  if l>1:
+    Left=arr[:l//2]
+    Right=arr[-l//2:]
+    merge_sort(Left)
+    merge_sort(Right)
+    for k in range(0, l):
+      if len(Left)>i:
+        if len(Right)>j:
+          if Left[i]<=Right[j]:
+            arr[k]=Left[i]
+            i=i+1
+          else:
+            arr[k]=Right[j]
+            j=j+1
+        else:
+          arr[k]=Left[i]
+          i=i+1
+      else: 
+        arr[k]=Right[j]
+        j=j+1
+  else:
+    return(arr)
+  
+  
+list=[6,5,4,3,2,45,7,88,9,1,1,1,1,4,-3]
+merge_sort(list)
 
-def merge_sort_tail(arr):
-    return arr
-
+print(list)
