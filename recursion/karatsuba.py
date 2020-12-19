4cf6eaad7f00af88aa6e2e86920c0ffc236dd785
Lisa <74136123+LizKrupets@users.noreply.github.com>
2020-12-19 18:29:58 +0300

    Update karatsuba.py

--- recursion/karatsuba.py
+++ recursion/karatsuba.py
@@ -1,15 +1,29 @@
-def rec_mult(x, y, n):
-    if n == 1:
-        print("n = 1: x = {0}, y = {1}".format(x, y))
-        return x*y
-    else: 
-        a = x // 10**(n//2)
-        b = x % 10**(n//2)
-        c = y // 10**(n//2)
-        d = y % 10**(n//2)
+def num_count(x):
+  num = 0
+  while(x > 10):
+    x //= 10
+    num += 1
+  num += 1
+  return num
 
-        print("n = {4}: a = {0}, b = {1}, c = {2}, d = {3}".format(a, b, c, d, n))
 
-        return 10**n * rec_mult(a, c, n//2) + 10**(n//2) * (rec_mult(a, d, n//2) + rec_mult(b, c, n//2)) + rec_mult(b, d, n//2)
+def karatsuba(x, y):
+  a = x // 10 ** (num_count(x) // 2)
+  b = x % 10 ** (num_count(x) // 2)
+  c = y // 10 ** (num_count(y) // 2)
+  d = y % 10 ** (num_count(y) // 2)
 
-print(rec_mult(9656, 8578, 4))
+  if (x == 10):
+      a = 1
+  if (y == 10):
+      c = 1
+
+  print(a, b, c, d)
+  
+  num = num_count(x)
+  res1 = a * c
+  res2 = b * d
+  res3 = (a + b) * (c + d)
+  res4 = res3 - res2 - res1
+  return(res1 * (10 ** num)) + (res4 * (10 ** (num // 2)) + res2)
+  
