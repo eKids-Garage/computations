558d3a60afc07a330d7ac3792de80d13aea62e8a
fedor <fedor.tryfanau@gmail.com>
2020-12-25 21:14:01 +0000

    ⠀

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -4,8 +4,50 @@
 # реализовать оба варианта
 
 def merge_sort(arr):
-    return arr
 
-def merge_sort_tail(arr):
-    return arr
+	
+		#divide
+	if len(arr) > 1:
+		L = arr[:len(arr)//2]
+		R = arr[len(arr)//2:]
+
+		L = merge_sort(L) # L
+		R = merge_sort(R) # R
+
+
+		#merge
+		K = []
+		
+		while L != [] and R != []:
+			
+			if L[0] >= R[0]:
+				K.append(R.pop(0))    #L > r
+
+			elif L[0] < R[0]:
+				K.append(L.pop(0))	  # R > L 
+		
+
+
+
+
+
+		
+		if L != [] or R != []:
+			if L != []:      
+				K.extend(L)  #L != []
+			if R != []:
+				K.extend(R)  #R !=[]
+
+
+		return K
+	return arr
+				
+
+
+
+
+
+
+print(merge_sort([0,21,435,1345,4351345,13450,46,246,5,134,54,6546,54,61,546,54601546,54,65,61,65,546,265,6,265,6,26562,6,5,62,6,]))
+
 
