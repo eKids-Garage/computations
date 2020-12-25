825f64d6e28501c8c26da7864839be2636956f33
Gleb Davidovich <klenka2017@gmail.com>
2020-12-25 19:56:17 +0000

    good

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -2,30 +2,33 @@
 # Попытайтесь реализовать ее как при помощи обычной, 
 # так и при помощи хвостовой рекурсии. Желательно, но необязательно
 # реализовать оба варианта
-def merge_sort(inp):
-  if len(inp) < 2:
+
+def merge_sort (inp):
+  if len(inp) == 1:
     return inp
-  L = merge_sort(inp[:len(inp)//2])
-  R = merge_sort(inp[len(inp)//2:])
+  else:
+    L = merge_sort(inp[:len(inp) // 2])
+    R = merge_sort(inp[len(inp) // 2:])
   return merge(L, R)
 
-def merge(L, R):
-  res = []
-  i1 = 0
-  i2 = 0
-  while i1 < len(L) and i2 < len(R):
-    if L[i1] < R[i2]:
-      res.append(L[i1])
-      i1+=1
+  return merge(L,R)
+
+def merge(L,R):
+  result = []
+  l1 = 0
+  r1 = 0
+  while l1 < len(L) and r1 < len(R):
+    if L[l1] < R[r1]:
+      result.append(L[l1])
+      l1+=1
     else:
-      res.append(R[i2])
-      i2+=1
-  if i2 < len(R):
-    res += R[i2:]
-  if i1 < len(L):
-    res += L[i1:]
-  return res
-
-print(merge_sort([4,7,1,9,2,3,6,5,8]))
+      result.append(R[r1])
+      r1+=1
+  if r1 < len(R):
+    result += R[r1:]
+  if l1 < len(L):
+    result += L[l1:]
+  return result
 
 
+print(merge_sort([2,1,4,3,8,7,5]))
