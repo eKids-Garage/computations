f1afee95f73564e1df80d75b5cb71ddb9689219d
DimaRedlin <dimaredlin715@gmail.com>
2020-12-25 16:41:53 +0000

    kek

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -3,9 +3,37 @@
 # так и при помощи хвостовой рекурсии. Желательно, но необязательно
 # реализовать оба варианта
 
-def merge_sort(arr):
-    return arr
+def mergeSort(arr):
+  if len(arr)>1:
+    mid = len(arr)//2
+    left = arr[:mid]
+    right = arr[mid:]
 
-def merge_sort_tail(arr):
-    return arr
+    mergeSort(left)
+    mergeSort(right)
+    i=0
+    j=0
+    k=0
+    while i<len(left) and j<len(right):
+      if left[i]<right[j]:
+        arr[k]=left[i]
+        i=i+1
+      else:
+        arr[k]=right[j]
+        j=j+1
+      k=k+1
 
+    while i<len(left):
+      arr[k]=left[i]
+      i=i+1
+      k=k+1
+
+    while j<len(right):
+      arr[k]=right[j]
+      j=j+1
+      k=k+1
+
+arr = [54,26,93,17,77,31,44,55,20,1]
+print(arr)
+mergeSort(arr)
+print(arr)
