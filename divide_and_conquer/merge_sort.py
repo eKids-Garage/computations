2f5534577412842e6f0a4e908deb0465ed36eb47
DimaRedlin <dimaredlin715@gmail.com>
2020-12-26 06:27:35 +0000

    .

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -11,27 +11,27 @@ def mergeSort(arr):
 
     mergeSort(left)
     mergeSort(right)
-    i=0
-    j=0
-    k=0
-    while i<len(left) and j<len(right):
-      if left[i]<right[j]:
-        arr[k]=left[i]
-        i=i+1
+    i = 0
+    j = 0
+    k = 0
+    while i < len(left) and j < len(right):
+      if left[i] < right[j]:
+        arr[k] = left[i]
+        i = i + 1
       else:
-        arr[k]=right[j]
-        j=j+1
-      k=k+1
+        arr[k] = right[j]
+        j = j + 1
+      k = k + 1
 
-    while i<len(left):
-      arr[k]=left[i]
-      i=i+1
-      k=k+1
+    while i < len(left):
+      arr[k] = left[i]
+      i = i + 1
+      k = k + 1
 
-    while j<len(right):
-      arr[k]=right[j]
-      j=j+1
-      k=k+1
+    while j < len(right):
+      arr[k] = right[j]
+      j = j + 1
+      k = k + 1
 
 arr = [54,26,93,17,77,31,44,55,20,1]
 print(arr)
