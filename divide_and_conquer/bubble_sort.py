e787b54efe160394810da75705b8d45adb417ebd
Maxim <catsofmax@gmail.com>
2021-01-16 19:10:10 +0300

    Solution

--- divide_and_conquer/bubble_sort.py
+++ divide_and_conquer/bubble_sort.py
@@ -1,6 +1,14 @@
-# Реализуйте алгоритм сортировки пузырьком. Можете попробовать реализовать его
-# как итеративно (используя цикл), так и рекурсивно.
+def sort_bubble(massiv): 
+    for i in range(len(massiv)-1): 
+        for j in range(0, len(massiv)-i-1): 
+            if massiv[j] > massiv[j+1] : 
+                e = massiv[j+1];
+                massiv[j+1] = massiv[j];
+                massiv[j] = e;
 
-def sort_bubble (arr):
-    return arr
-
+massiv = [1, 2, 5, 13, 8, 3, 7];
+sort_bubble(massiv);
+i = 0;
+while i < len(massiv):
+  print(massiv[i] , ' ');
+  i = i + 1
