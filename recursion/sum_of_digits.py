74cd8d6de385e89bfafc2a3c5a735acaf9dea258
Yulia Kanoplich <yulia.kanoplich@gmail.com>
2021-01-16 14:46:39 +0000

    homework2

--- recursion/sum_of_digits.py
+++ recursion/sum_of_digits.py
@@ -3,14 +3,10 @@
 n=int(input())
 
 def sum(N):
-  summa = 0
-  digit = 0
-  while N>0:
-    digit = N%10
-    summa = summa+digit
-    N = N//10
-  print(summa)
-    
+  if N < 10:
+    return N
+  else:
+    return N % 10 + sum(N // 10)   
 
 
-sum(n)
+print(sum(n))
