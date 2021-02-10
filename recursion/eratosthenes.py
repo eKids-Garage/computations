d76259ef746ef86f9c5f6fe4152563d7df0822aa
Lisa <74136123+LizKrupets@users.noreply.github.com>
2021-02-10 20:43:49 +0300

    Update eratosthenes.py

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -1,8 +1,21 @@
-# Написать программу, которая находит все простые числа в заданном 
-# интервале методом "Решето Эратосфена". 
-# start и finish - границы диапазона (включительно).
-
 def sieve(start, finish):
-    primes = []
-    return primes
+  primes = []
+  res = []
+  for i in range(2, finish + 1):
+    primes.append(i)
 
+
+  for n in primes:
+    
+    if n ** 2 > finish:
+      for num in primes:
+        if num >= start:
+          res.append(num)
+        else:
+          continue
+      return res
+    
+    for num in primes[n * 2:]:
+      if num % n == 0:
+        primes.remove(num)
+ 
