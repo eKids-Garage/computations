81ed53c7b072ffffb0c1082a9415c5a2b11fc2d7
Dzmitry K <dzmitry.kuksau1@gmail.com>
2021-02-10 20:48:04 +0000

    joseph - task done

--- recursion/joseph.py
+++ recursion/joseph.py
@@ -5,10 +5,27 @@
 # 2. survive_num(n, k) - без использования массива 
 
 def survive(n, k):
-    return []
-
-
-def survive_num(n, k):
-    pos = 0
     
-    return pos
+    # создаём массив, значение элемента равно индексу, начало с 0
+    arr = []
+    for i in range(0, n):
+        arr.append(i)
+    #print(arr)
+
+    k = k - 1 # если каждый третий, то убиваем через 2
+    next_kill = 0
+
+    while (len(arr) > k):
+        next_kill += k
+
+        # если массив закончился, то переходит на начало
+        if (next_kill >= len(arr)):
+            next_kill = next_kill - len(arr)
+        
+        # удаляем из массива, размер станет меньше на 1
+        del arr[next_kill]
+        #print(arr)
+    
+    return arr
+
+print(survive(41, 2))
