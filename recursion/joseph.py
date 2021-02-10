3cd7e5b9cef47cfa5a90b3096fa8d5d19607bb7e
Lendrina <elena.suboch5@gmail.com>
2021-02-10 20:41:34 +0000

    Something

--- recursion/joseph.py
+++ recursion/joseph.py
@@ -3,12 +3,15 @@
 # 
 # 1. survive(n, k) - используя массив. 
 # 2. survive_num(n, k) - без использования массива 
-
-def survive(n, k):
-    return []
-
+n = int(input('Число солдат:'))
+k = int(input('Шаг:'))
 
 def survive_num(n, k):
-    pos = 0
+  if n == 1:
+    return 1
+  return 1 + (survive_num(n-1, k) + k - 1) % n
+print('Место под номером ', survive_num(n, k))
+
+def survive(n, k):
     
-    return pos
+    return 
