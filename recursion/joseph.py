de660d97f689c9d83db7e4c47cbd9acfae495439
MishaKulik <45013338+MishaKulik@users.noreply.github.com>
2021-02-10 23:16:32 +0300

    Update joseph.py

--- recursion/joseph.py
+++ recursion/joseph.py
@@ -4,8 +4,13 @@
 # 1. survive(n, k) - используя массив. 
 # 2. survive_num(n, k) - без использования массива 
 
-def survive(n, k):
-    return []
+def survive(n, k, i = 0, arr= []):
+  if not i:
+    arr = [num+1 for num in range(n)]
+  if len(arr) < 2:
+    return arr
+  arr.remove(arr[(i + k - 1)%len(arr)])
+  return survive(n, k, i+1, arr)
 
 
 def survive_num(n, k):
