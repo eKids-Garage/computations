432b1e9db66a1c671e44983f6b2d00525508d9fd
MishaKulik <45013338+MishaKulik@users.noreply.github.com>
2021-02-10 22:35:30 +0300

    Update eratosthenes.py

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -1,8 +1,15 @@
 # Написать программу, которая находит все простые числа в заданном 
 # интервале методом "Решето Эратосфена". 
 # start и finish - границы диапазона (включительно).
-
 def sieve(start, finish):
-    primes = []
-    return primes
-
+  arr = [num for num in range(2,finish)]
+  for el in arr:
+    if el > 0:
+      for ind in range(el*2, len(arr), el):
+          arr[ind] = 0
+  def filt(num):
+    return num!=0
+  arr = list(filter(filt,arr))
+  for i in range(len(arr)):
+    if arr[i] >= start:
+      return arr[i:]
