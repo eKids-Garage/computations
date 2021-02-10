dc9a5c230b8bc6af9513e625cd42a1bfeafd5a2e
Adam Sidoryk <adam.sidoryk@gmail.com>
2021-02-10 16:34:16 +0000

    solution

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -3,6 +3,15 @@
 # start и finish - границы диапазона (включительно).
 
 def sieve(start, finish):
-    primes = []
-    return primes
+  i = 0
+  primes = list(range(2,finish+1))
+  while i < len(primes)//2:
+    for j in range(i + primes[i], len(primes),primes[i]):
+      primes[j] = 0
+    i = i + 1
+    while primes[i] == 0 and i < len(primes):
+      i = i + 1  
+  primes = [x for x in primes if x!=0]
+  return primes
 
+print(sieve(0,28))
