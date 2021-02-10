963497a90dfa06e48d23aa8687844bc81c00a09d
IvanSuboch <ivan.suboch9@gmail.com>
2021-02-10 20:44:07 +0000

    I'm done!

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -2,7 +2,19 @@
 # интервале методом "Решето Эратосфена". 
 # start и finish - границы диапазона (включительно).
 
-def sieve(start, finish):
-    primes = []
-    return primes
+import math
 
+def sieve(start, finish):
+  primes = []
+  max_prime = round(math.sqrt(finish))
+  for n in range(start, finish + 1):
+    is_prime = True
+    for p in range(2, max_prime + 1):
+      if n % p == 0:
+        is_prime = False
+        break
+    if is_prime:
+      primes.append(n)
+  return primes
+
+print(sieve(10, 20))
