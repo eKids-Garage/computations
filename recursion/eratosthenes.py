1466115ba5464460b0c50e026f8a5466d6cd8fd1
ivnb <6042430@mail.ru>
2021-02-10 18:47:14 +0000

    up to 100000

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -1,8 +1,28 @@
 # Написать программу, которая находит все простые числа в заданном 
 # интервале методом "Решето Эратосфена". 
 # start и finish - границы диапазона (включительно).
+import math
+set=[]
+primes=[]
 
 def sieve(start, finish):
-    primes = []
-    return primes
+  if (start==1):
+    sieve(2,finish)
+  if start<math.sqrt(finish):
+    if set==[]:
+      for i in range (start, finish+1):
+        set.append(i)
+    
+    if start!=0:
+     for n in range (2*start-1, finish, start): 
+       set[n-1]=0
+     sieve(start+1,finish)
+  else:
+    for t in range(0, finish-1):
+      if set[t]!=0:
+        primes.append(set[t])
+    print(primes)
 
+sieve(2,100000)
+
+#works much better with sqrt than with finish/2. for /2 recursion crashes at 500(even less) and with sqrt only at 10000000
--- recursion/joseph.py
+++ recursion/joseph.py
@@ -3,12 +3,32 @@
 # 
 # 1. survive(n, k) - используя массив. 
 # 2. survive_num(n, k) - без использования массива 
-
+surv=[]
 def survive(n, k):
-    return []
+  for i in range (0, n):
+    surv.append(i+1)
+  for j in range (k-1,n-1,k):
+    for a in range (0, k-1):
+      if surv[j+a]!=0:
+        surv[j+a]=0
+        break
+  for j in range (k-1,n-1,k):
+    for a in range (0, k-1):
+      if surv[j+a]!=0:
+        surv[j+a]=0
+        break
+  print(surv)
+
+    
+
+    
 
 
 def survive_num(n, k):
     pos = 0
     
     return pos
+    
+survive(3, 2)
+
+
