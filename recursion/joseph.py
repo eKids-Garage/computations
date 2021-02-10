82154602208a25e0fcb59652c671c1a6bfdb035a
Valentinhilko <garageepam12367@gmail.com>
2021-02-11 00:04:13 +0300

    some comments to file

--- recursion/joseph.py
+++ recursion/joseph.py
@@ -1,14 +1,48 @@
-# Реши задачу Иосифа Флавия:
-# где n - число солдат, k - шаг (2 - каждый второй (сосед), 3 - каждый третий и т.д.)
-# 
-# 1. survive(n, k) - используя массив. 
-# 2. survive_num(n, k) - без использования массива 
 
-def survive(n, k):
-    return []
+# i это индекс элементов в массиве 
+# n это элемент который убьет следующего не нулевого
+# '-1' - значит все готово
+
+def find_next_alive(array, n):
+    if  len(array) < 2 :
+        print('Little array')
+        return -1
+    else:
+        i = n + 1
+        if n == len(array) -1:
+            i = 0 
+        while array[i] == 0: 
+            if i == len(array) -1:
+                i = -1
+            i = i + 1 # получится 0 (что нам и нужно)
+        if n == i:
+            return -1
+        else:   
+            return i    
 
 
-def survive_num(n, k):
-    pos = 0
-    
-    return pos
+def kill(array, n, i):
+    i = i + 1
+    next = find_next_alive(array,n)# тот кто умрет
+    if next == -1:
+        return array #'Stop'
+    array[next] = 0
+    killer = find_next_alive(array,n) # тот кто потом убьет
+    if killer == (-1):
+        return 'Stop'
+    else:
+        return kill(array, killer,i)
+
+''' Oбьявляем переменные , запускаем функции '''
+array = []
+number = int(input("Введите кол-во воинов : ")) 
+for a in range (1,number + 1):
+    array.append(a)
+print('----', array)
+n = 0
+kill(array, n,0)
+
+# преобразуем во множество чтоб убрать нули
+res = set(array)
+res.remove(0)
+print('survive ',*res) # 'сам элемент
