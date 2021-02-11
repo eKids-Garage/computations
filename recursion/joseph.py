7f832f55e56518e6dd8dc42ce815919cf8d1e3a4
ivnb <6042430@mail.ru>
2021-02-11 16:07:17 +0000

    josephus

--- recursion/joseph.py
+++ recursion/joseph.py
@@ -4,11 +4,46 @@
 # 1. survive(n, k) - используя массив. 
 # 2. survive_num(n, k) - без использования массива 
 
-def survive(n, k):
-    return []
+
+
+surv=[]
+def survive(n, k, j=0):
+  if surv==[]:
+    for i in range (0, n):
+      surv.append(i+1)
+      j=k
+  count=0
+  sortsurv=[]
+  for i in range (0,n):
+    if not surv[i]==0:
+      count=count+1
+      sortsurv.append(surv[i])
+  if count==1:  
+    print (sortsurv) 
+  else:
+    for a in range (0,n):
+      if not surv[a]==0:
+        if j==1:
+          surv[a]=0
+          j=k
+        else:
+          j=j-1
+    survive(n,k,j)
+
+    
+survive(10,3)
+ 
+  
+
+
+    
+
+    
 
 
 def survive_num(n, k):
     pos = 0
     
     return pos
+    
+
