9245d57b96644c8bb58e4e5c171c7dc958bbd388
Maria Zatsepina <zatsepina.maria@gmail.com>
2021-02-11 21:18:26 +0000

    Я хотела сначала отдельным реплом, но что-то не сложилось. Поэтому я просто удалила весь код и перенесла сюда мой)

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -2,19 +2,39 @@
 # интервале методом "Решето Эратосфена". 
 # start и finish - границы диапазона (включительно).
 
-import math
+print('enter the first number')
+num1 = int(input())
+print('enter the last number')
+num = int(input())
+
 
 def sieve(start, finish):
-  primes = []
-  max_prime = round(math.sqrt(finish))
-  for n in range(start, finish + 1):
-    is_prime = True
-    for p in range(2, max_prime + 1):
-      if n % p == 0:
-        is_prime = False
-        break
-    if is_prime:
-      primes.append(n)
-  return primes
+  numbers = []
+  while start<=finish:
+    numbers.append(start)
+    start = start+1
+  #print(numbers)
 
-print(sieve(10, 20))
+  i = 0
+  numtest = 2
+  if 1 in numbers:
+    numbers.remove(1)
+  else:
+    breakpoint
+  while numtest < numbers[len(numbers) - 2]:
+    while len(numbers) > i:
+      if numbers[i] != numtest and numbers[i]%numtest == 0:
+        numbers.pop(i)
+      else:
+        i = i + 1
+
+    i = 0
+    for nextnum in range(len(numbers)):
+      if numbers[nextnum] > numtest:
+        numtest = numbers[nextnum]
+      else:
+        breakpoint
+  return numbers
+
+print("here is the prime number in between", num1, "and", num, sieve(num1, num))
+
