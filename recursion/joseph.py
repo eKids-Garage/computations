0cb6b76a724c977f7355e4b6a631a862cf252f3f
Retr0 <kirill.nikitin@outlook.com>
2021-02-12 16:42:30 +0000

    recursion/joseph.py was done and .replit was changed to do this tasks

--- recursion/joseph.py
+++ recursion/joseph.py
@@ -4,11 +4,35 @@
 # 1. survive(n, k) - используя массив. 
 # 2. survive_num(n, k) - без использования массива 
 
+#initialization
+n = int(input("Type the number of warriors:"))
+k = int(input("Type the step:"))
+i = 0
+main_list = []
+
+
+#making list with warriors
+for any_var in range(n + 1):
+    main_list.append(any_var)
+main_list.remove(0)    
+
+
+#nice outputing of the initial list
+print("Initial list is:")
+print(*main_list, sep=", ")
+
+
+#main function
 def survive(n, k):
-    return []
-
-
-def survive_num(n, k):
-    pos = 0
     
-    return pos
+    global i
+    
+    if len(main_list) < 2:
+        return main_list
+    else:
+        main_list.remove(main_list[(i + k - 1) % len(main_list)])
+        i += 1
+        return survive(n, k)
+    
+
+print("\nThe survivor is warrior with number", *survive(n, k), sep=" ")
