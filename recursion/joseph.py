f33fd749b85c64e5d178ef3307bf052d552a2ff5
Adam Sidoryk <adam.sidoryk@gmail.com>
2021-02-12 17:06:09 +0000

    solution

--- recursion/joseph.py
+++ recursion/joseph.py
@@ -5,8 +5,26 @@
 # 2. survive_num(n, k) - без использования массива 
 
 def survive(n, k):
-    return []
+  a = list(range(1,n + 1))
+  while len(a) > k - 1:
+    b = []
+    i = 0
+    n = len(a)
+    print("round:")
+    while i < n:
+      m = min(i+k-1, n)
+      print(a[i:m+1])
+      if i+k < n:
+        b += a[i:m]
+      else:
+        b = a[i:m] + b
+      i += k
+    print(b)
+    a = b
+  return a
 
+print(survive(9,2))
+# print(survive(16,3))
 
 def survive_num(n, k):
     pos = 0
