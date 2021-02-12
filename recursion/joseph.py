714051e3a887f79e8f7be2f80cf650adccbdff89
lendrina <73532978+lendrina@users.noreply.github.com>
2021-02-12 23:50:26 +0300

    Update joseph.py

--- recursion/joseph.py
+++ recursion/joseph.py
@@ -6,7 +6,32 @@
 
 def survive_num(n, k):
 
+n = int(input('Число солдат:'))
+k = int(input('Шаг:'))
 
 def survive(n, k):
-    
-    return 
+  i = 1
+  mass = []
+  while i < n + 1:
+    mass.append(i)
+    i = i + 1
+  number = 0
+  count = 0
+  while count < n-1:
+    for element in mass:
+      if element == 0:
+        continue
+      number = number + 1
+      if number % 4 == 0:
+        v = mass.index(element)
+        mass[v] = 0
+        print(mass)
+        count = count + 1
+      continue
+  end = []
+  for x in mass:
+    if x!= 0:
+      end.append(x)
+  return end  
+
+print(survive(n, k))
