a9e34dd5bd27ccd9bbb88a76f6e3ba84171162df
Valentinhlko <73731049+Valentinhlko@users.noreply.github.com>
2021-02-12 15:02:44 +0300

    Update prime_div.py

--- recursion/prime_div.py
+++ recursion/prime_div.py
@@ -1,13 +1,13 @@
+
 N = int(input("Введи число  : "))
 
 def prime_div(N, K = 2):
   if N == K :  # n%k !=0 не работает правильно
-    print(K)
-    return
+    return K
   elif N % K == 0:
     print(K)
-    prime_div(N // K, K)
+    return prime_div(N // K, K)
   else:
-    prime_div(N, K + 1)
+    return prime_div(N, K + 1)
 
-prime_div(N)
+print(prime_div(N))
