7862b355ab8d0a20f962aa0014c60f25ba4f24ba
Lendrina <elena.suboch5@gmail.com>
2021-02-12 18:32:09 +0000

    All

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -2,7 +2,34 @@
 # интервале методом "Решето Эратосфена". 
 # start и finish - границы диапазона (включительно).
 
-def sieve(start, finish):
-    primes = []
-    return primes
+start = int(input('Начало отсчета:'))
+finish = int(input('Конец отсчета:'))
 
+def sieve(start, finish):
+  i = start
+  mass = []
+  while i < finish + 1:
+    mass.append(i)
+    i = i + 1
+  for p in mass:
+    def is_prime(n, c = 2):
+      if c > int(n**0.5) :
+        r = "YES"
+        return r
+      if n % c == 0:
+        r = "NO" 
+        return r
+      return is_prime(n, c + 1)
+    z = is_prime(p)
+    print(z)
+    if z == "NO":
+      v = mass.index(p)
+      mass[v] = 0
+      continue
+  primes = []
+  for x in mass:
+    if x!= 0:
+      primes.append(x)
+  return primes
+
+print(sieve(start, finish))
