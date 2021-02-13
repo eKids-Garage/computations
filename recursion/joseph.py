012f280858d381dab2e92cf5c62e11dfbb7d8e17
Lisa <74136123+LizKrupets@users.noreply.github.com>
2021-02-13 11:12:03 +0300

    Update joseph.py

--- recursion/joseph.py
+++ recursion/joseph.py
@@ -5,3 +5,18 @@ def flav(w, k):
     index = (index + k - 1) % len(w) 
     w.pop(index)
   return w[0]
+
+
+# n всегда равно 2
+def survive(n, k):
+  if n == 1:
+    return 1
+  fn = 2
+  sn = 1
+  while fn < n:
+    fn += 1
+    if sn + 2 > fn:
+      sn = 1
+    else:
+      sn += 2
+  return sn
