92eb675cf76fae5802aebacaca069358c868113d
Nikita Gapankov <gapankovnl@gmail.com>
2021-02-13 08:15:56 +0000

    solution

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -6,6 +6,15 @@ import math
 
 def sieve(start, finish):
   primes = []
+  for n in range(start, finish + 1):
+    is_prime = True
+    for p in range(2, round(math.sqrt(n)) + 1):
+      if n % p == 0:
+        is_prime = False
+        break
+    if is_prime:
+      primes.append(n)
   return primes
-
-print(sieve(10, 20))
+start = int(input())
+finish = int(input())
+print(sieve(start, finish))
