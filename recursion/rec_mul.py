c0bc71c93d0f733b7c485f635c0d71a1db5b68da
Adam Sidoryk <adam.sidoryk@gmail.com>
2021-03-19 16:54:41 +0000

    solution

--- recursion/rec_mul.py
+++ recursion/rec_mul.py
@@ -3,5 +3,18 @@
 # но также можно ограничиться числами с размерностью 
 # степень двойки: 2, 4, 8, 16 и т.д.
 
-def rec_mul(a, b):
-    return a * b
+def rec_mul(x, y, n): 
+  if n == 1:
+    print("n = 1:x = {0}, y = {1}".format(x, y))
+    return x*y
+  else:
+    a = x // 10**(n//2)  
+    b = x % 10**(n//2)      
+    c = y // 10**(n//2)      
+    d = y % 10**(n//2)      
+
+    print("n = {4}: a = {0}, b = {1}? c = {2}, d = {3}".format(a, b, c, d, n))
+
+    return 10**n * rec_mul(a, c, n//2) + 10**(n//2) * (rec_mul(a, d, n//2) + rec_mul(b, c, n//2)) + rec_mul(b, d, n//2)
+
+print(rec_mul(9656, 8578, 4))
