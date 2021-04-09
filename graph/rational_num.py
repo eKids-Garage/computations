1bfab6c6e2c8955afb39fc03d7c22f485877ca7a
Adam Sidoryk <>
2021-04-09 15:31:24 +0000

    solution

--- graph/rational_num.py
+++ graph/rational_num.py
@@ -1,6 +1,32 @@
 # Напишите функцию, которая складывает два рациональных числа
 # Структуру данных для хранения чисел выберите и реализуйте сами
-
+n1 = int(input("write numerator 1 - "))
+d1 = int(input("write denaminator 1 - "))
+n2 = int(input("write numerator 2 - "))
+d2 = int(input("write denaminator 2 - "))
+a = [n1,d1]
+b = [n2,d2]
 
 def sum_rat(a, b):
-    return 0
+  s = a[0] * b[1] + b[0] * a[1]
+  p = a[1] * b[1]
+  print(s,p)
+  e = euclidus(s, p)
+  print(e)
+  s = s // e
+  p = p // e
+  print("answer:", s,p)
+
+def euclidus(a, b):
+  print(a, b)
+  if a != b:    
+    if a > b:
+      a -= b
+    else:
+      b -= a
+    return euclidus(a,b)
+
+  else:
+    return b
+    
+sum_rat(a, b)
