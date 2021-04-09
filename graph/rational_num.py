ce095322eb5aafc7d4763b0f2a01e6c0c28aea66
Gleb Davidovich <>
2021-04-09 20:07:09 +0000

    .

--- graph/rational_num.py
+++ graph/rational_num.py
@@ -1,6 +1,32 @@
 # Напишите функцию, которая складывает два рациональных числа
 # Структуру данных для хранения чисел выберите и реализуйте сами
 
+def gcd (a,b):
+  if a != b:
+    if a > b:
+      a -= b
+    else:
+      b -= a
+    return gcd(a,b)
+  else:
+    return b
 
 def sum_rat(a, b):
-    return 0
+  n = a[0] * b[1] + b[0] * a[1]
+  d = a[1] * b[1]
+  e = gcd(n,d)
+  n = n // e
+  d = d // e
+  if (n // d) > 1:
+    p = n // d
+    n = n - p * d
+    print(p,n,'/',d)
+  else:
+    print(n,'/',d)
+
+
+#первое число
+a = [5,13]
+#второе число
+b = [15,3]
+sum_rat(a,b)
