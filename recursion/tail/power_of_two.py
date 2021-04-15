242ef24e5db93168ffabaa8aaaf46a30e928520e
Nikita Kabeshev <>
2021-04-15 17:19:49 +0000

    power of two tail almost done

--- recursion/tail/power_of_two.py
+++ recursion/tail/power_of_two.py
@@ -4,10 +4,27 @@
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
 def is_power_of_two(N):
-    return 'YES'
+  return 'YES'
 
 
-def is_power_of_two_tail(N, n):
-    return 'YES'
+def is_power_of_two_tail(N, powered=1):
+	if powered == N:
+		return "YES"
+	if powered > N:
+		return "NO"
+	powered = powered*2	 
+	return is_power_of_two(N,powered)
 
+N=int(input("Enter number \n"))	
+print(is_power_of_two(N))
 
+powered = 1
+while True:
+	if powered==N:
+		print("YES")
+		break
+	if powered>N:
+		print("NO")
+		break
+	powered=powered*2
+	
