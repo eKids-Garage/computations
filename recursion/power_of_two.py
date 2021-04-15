7cc0cbf5ce722ee10cfc2a9b7f3daaaed7dfd30a
Nikita Kabeshev <>
2021-04-15 15:48:45 +0000

    power of two done (prime_number не изменён)

--- recursion/power_of_two.py
+++ recursion/power_of_two.py
@@ -1,5 +1,13 @@
 #Дано натуральное число N. Выведите слово YES, если число N является точной степенью двойки, или слово NO в противном случае.
 #Операцией возведения в степень пользоваться нельзя!
 
-def is_power_of_two(N):
-    return 'YES'
+def is_power_of_two(N, powered=1):
+	if powered == N:
+		return "YES"
+	if powered > N:
+		return "NO"
+	powered = powered*2	 
+	return is_power_of_two(N,powered)
+
+number=int(input("Enter number \n"))	
+print(is_power_of_two(number))
