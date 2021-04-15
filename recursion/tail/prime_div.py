245bdc8b90a64718d9ec34fcc98bc11ac7e5e11f
Nikita Kabeshev <>
2021-04-15 16:10:33 +0000

    prime_div done

--- recursion/tail/prime_div.py
+++ recursion/tail/prime_div.py
@@ -2,9 +2,38 @@
 # Оформите в виде обычной и хвостовой рекурсии
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
-def dividors(N):
-    return 0
+def dividors(n,k=2):
+	if n/k<k:
+		return [n]
+	if n%k == 0:
+		return [k] + dividors(n//k)
+	if n%k !=0:	
+		k=k+1
+		return dividors(n,k)
+
+    
 
 
-def dividors_tail(N, k):
-    return 0
+def divide_tail(N, k=2):
+	if N==1:
+		return 0
+	if N%k == 0:
+		print(k)
+		N=N//k
+		return divide_tail(N)
+	if N%k != 0:
+		k=k+1
+		return divide_tail(N,k)
+
+print(dividors(24))
+divide_tail(24)
+N=24
+
+k=2
+while N>1:
+	if N%k==0:
+		print(k)
+		N=N//k
+		k=2
+	else:
+		k=k+1	
