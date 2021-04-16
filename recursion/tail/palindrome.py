21c47a5bad02d2c3f8816e71d6172a74ab0dbef2
Nikita Kabeshev <>
2021-04-16 09:22:39 +0000

    palindrome tail done

--- recursion/tail/palindrome.py
+++ recursion/tail/palindrome.py
@@ -4,8 +4,36 @@
 # Вариант с хвостовой рекурсией преобразуйте в цикл while
 
 def is_palindrome(word):
-    return "YES"
+	  return "YES"
 
 
-def is_palindrome_tail(word, n = 0):
-    return "YES"
+def is_palindrome_tail(word, c=0):
+	Len = len(word)
+
+	if word[Len-c-1] == word[c]:
+		if c == Len//2:
+			return "YES"
+		else:	
+			c = c + 1
+			return is_palindrome(word,c)
+	else:
+		return "NO"	
+
+
+
+slovo = input("Enter Word \n")
+print(is_palindrome_tail(slovo))
+Len = len(slovo)
+c=0
+
+while True:
+	if slovo[Len-c-1] != slovo[c]:
+		print("NO")
+		break
+	if c==Len//2:
+		print("YES")	
+		break
+	else:
+		c=c+1
+
+
