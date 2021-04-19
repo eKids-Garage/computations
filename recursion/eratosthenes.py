f30c01ba9213c570263b4ed5e7a098dcfe76f9e1
Nikita Kabeshev <>
2021-04-19 13:24:34 +0000

    eratosthenes done

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -2,10 +2,31 @@
 # интервале методом "Решето Эратосфена". 
 # start и finish - границы диапазона (включительно).
 
-import math
+#import math
 
 def sieve(start, finish):
-  primes = []
-  return primes
+	if start<finish:
+		#Create number table
+		numbers = []
+		i=start
+		while i <= finish:
+			numbers.append(i)
+			i=i+1
+		#######
+
+
+		for curret in range(2, finish):
+			power=2
+			while curret*power<=finish :
+				CurPow = curret*power
+				if CurPow in numbers:
+					print(curret, power, CurPow)
+					numbers.remove(CurPow)
+				power=power+1
+
+		return numbers
+	else:
+		return "start > finish !!! Change values"
 
 print(sieve(10, 20))
+
