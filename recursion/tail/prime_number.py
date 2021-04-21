cce97e3085bb809326fac2c26ef93dad051acef4
Nikita Kabeshev <>
2021-04-21 15:30:43 +0000

    prime_number tail done

--- recursion/tail/prime_number.py
+++ recursion/tail/prime_number.py
@@ -7,5 +7,26 @@ def is_prime(n):
   return("YES")
 
     
-def is_prime_tail(n, k):
-  return("YES")
+def is_prime_tail(N, delitel=2):
+	if delitel == N:
+		return "YES"
+	if N%delitel ==0 :
+		return "NO"#,delitel
+	else:
+		delitel = delitel + 1
+		return is_prime_tail(N,delitel)
+
+number=int(input("Enter number \n"))
+print(is_prime_tail(number))    
+
+delitel=2
+N=number
+while True:
+	if delitel == N:
+		print("YES")
+		break
+	if N%delitel == 0:
+		print("NO")
+		break
+	delitel = delitel + 1
+
