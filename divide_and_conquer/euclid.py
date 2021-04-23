a966655d075eeab61dc2ace62723f52c92f4ea4c
Nikita Kabeshev <>
2021-04-23 11:33:51 +0000

    euclidus done

--- divide_and_conquer/euclid.py
+++ divide_and_conquer/euclid.py
@@ -1,4 +1,11 @@
 # Реализуйте алгоритм Евклида с помощью рекурсии
 
 def euclidus(a, b):
-    return a
+	if a==b:
+		return a
+	if a>b:
+		return euclidus(a-b,b)
+	if a<b:
+		return euclidus(a,b-a)		
+
+print(euclidus(10,15))		
