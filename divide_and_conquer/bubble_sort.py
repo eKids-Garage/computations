56278a87e83ae3c64a6afd19d18cb383405efe18
Nikita Kabeshev <>
2021-04-23 14:48:02 +0000

    bubble sort done

--- divide_and_conquer/bubble_sort.py
+++ divide_and_conquer/bubble_sort.py
@@ -2,5 +2,17 @@
 # как итеративно (используя цикл), так и рекурсивно.
 
 def sort_bubble (arr):
-    return arr
+	Sort_list = [0]
+	for element in arr:
+		for curret in range(0,len(Sort_list)):
+			#print(curret,element,Sort_list)
+			if element < Sort_list[curret] or element == Sort_list[curret]:
+				Sort_list.insert(curret, element)
+				break
+			if curret == len(Sort_list)-1:
+				Sort_list.insert(-1,element)
+	Sort_list.pop()			
+	return Sort_list
 
+N=[6,1,2,4,5,3,7,32]
+print(sort_bubble(N))
