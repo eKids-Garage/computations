d0a9f3dbdb21f7a5725c6525a6f9fc065d305d45
Lisa <74136123+LizKrupets@users.noreply.github.com>
2021-04-28 21:33:47 +0300

    Update adj_list.py

--- graph/adj_list.py
+++ graph/adj_list.py
@@ -1,16 +1,24 @@
-# Graph A: Non-directed graph
-# Vertices:
-graph_a = [[3, 4], [2, 4]] # Complete the adjacency list for the Graph A
-
-
-# Graph B: Non-directed graph
-# Vertices:
-graph_b = [[3, 4], [4]] # Complete the adjacency list for the Graph B
+graph_a = [[3, 4], [2, 4], [1, 3, 5], [0, 2, 5], [0, 1, 5], [2, 3, 4]]
+graph_b = [[3, 4], [4], [1], [2], [5], [3, 2]]
 
 
 def is_adjacent(graph, vert_a, vert_b):
-    #Complete the function
-    return False
+	arr = graph[vert_a]
+
+	while len(arr):
+		for vert in range(len(arr) - 1, -1,-1):
+			if arr[vert] == vert_a:
+				return False
+			elif arr[vert] == vert_b:
+				return True
+		
+		for i in range(len(arr)):
+			ind = arr[0]
+			del arr[0]
+			arr += graph[ind]
+		
+
+	return False
 
 
 print(is_adjacent(graph_a, 5, 2))
