684fcfd9e62da9bb4218c7a3cd6691b38fe4fef4
Nikita Listopadov <>
2021-05-01 17:55:49 +0000

    I'm done

--- graph/adj_list.py
+++ graph/adj_list.py
@@ -1,18 +1,29 @@
+import random
+
 # Graph A: Non-directed graph
 # Vertices:
-graph_a = [[3, 4], [2, 4]] # Complete the adjacency list for the Graph A
+graph_a = [[3,4],[2,4],[1,3,5],[0,2,5],[0,1,5],[2,3,4]]
 
 
 # Graph B: Directed graph
 # Vertices:
-graph_b = [[3, 4], [4]] # Complete the adjacency list for the Graph B
+graph_b = [[3, 4], [4],[1],[2],[5],[2,3]]
 
 
 def is_adjacent(graph, vert_a, vert_b):
-    #Complete the function
-    return False
-
-
+	och = graph[vert_a]
+	ver = []
+	while (len(och) > 0):
+		if (och[0] == vert_b):
+			return True
+		elif (och[0] not in ver):
+			ver.append(och[0])
+			for i in range (len(graph[och[0]])):
+				och.append(graph[och[0]][i])
+			och.pop(0)
+		else:
+			och.pop(0)
+	return False
 
 print(is_adjacent(graph_a, 5, 2))
 print(is_adjacent(graph_a, 0, 3))
@@ -23,3 +34,6 @@ print(is_adjacent(graph_b, 5, 2))
 print(is_adjacent(graph_b, 0, 3))
 print(is_adjacent(graph_b, 3, 4))
 print(is_adjacent(graph_b, 2, 5))
+print()
+print(is_adjacent(graph_b, random.randint(0, 5), 0))
+
