1bb49b2f9148e007ca4da3c731912cf7fe5e34ae
Lisa <74136123+LizKrupets@users.noreply.github.com>
2021-05-06 19:57:18 +0300

    Update adj_list.py

--- graph/adj_list.py
+++ graph/adj_list.py
@@ -3,7 +3,8 @@ graph_b = [[3, 4], [4], [1], [2], [5], [3, 2]]
 
 
 def is_adjacent(graph, vert_a, vert_b):
-	arr = graph[vert_a]
+	arr = [graph[vert_a][i] for i in range(len(graph[vert_a]))]
+
 
 	while len(arr):
 		for vert in range(len(arr) - 1, -1,-1):
