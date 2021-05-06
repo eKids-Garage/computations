09ff11521c5fa1f2b6c2d743c391b66a54a2044f
ivnb <>
2021-05-06 19:15:27 +0000

    .

--- graph/adj_matrix.py
+++ graph/adj_matrix.py
@@ -3,6 +3,7 @@
           #  0  1  2  3  4  5  6  7
 graph_a = [ [0, 0, 0, 1, 1, 0, 1, 0],  #  0
             [0, 0, 0, 0, 0, 0, 0, 0],  #  1
+            [0, 1, 0, 0, 0, 1, 0, 0],  #  2
             [0, 0, 0, 0, 0, 0, 0, 0],  #  3
             [0, 0, 0, 0, 0, 0, 0, 0],  #  4
             [0, 0, 0, 0, 0, 0, 0, 0],  #  5
@@ -17,6 +18,7 @@ graph_a = [ [0, 0, 0, 1, 1, 0, 1, 0],  #  0
           #  0  1  2  3  4  5  6  7
 graph_b = [ [0, 0, 0, 1, 1, 0, 1, 0],  #  0
             [0, 0, 0, 0, 0, 0, 0, 0],  #  1
+            [0, 1, 0, 0, 0, 0, 0, 0],  #  2
             [0, 0, 0, 0, 0, 0, 0, 0],  #  3
             [0, 0, 0, 0, 0, 0, 0, 0],  #  4
             [0, 0, 0, 0, 0, 0, 0, 0],  #  5
@@ -26,7 +28,9 @@ graph_b = [ [0, 0, 0, 1, 1, 0, 1, 0],  #  0
 
 
 def is_adjacent(graph, vert_a, vert_b):
-    #Complete the function
+  if graph[vert_a][vert_b]==1:
+    return True
+  else: 
     return False
 
 
@@ -40,4 +44,5 @@ print("Graph B: ")
 for i in range(0, 8):
     for j in range(0, 8):
         print("Vertice {0} and vertice {1} are adjacent: {2}".format(i, j, is_adjacent(graph_b, i, j)))        
+
 
