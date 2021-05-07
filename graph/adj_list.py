f3f1e02debb5c2d33a5d983c88984464f2a44deb
Rominator <>
2021-05-07 19:54:27 +0000

    l

--- graph/adj_list.py
+++ graph/adj_list.py
@@ -1,16 +1,26 @@
 # Graph A: Non-directed graph
 # Vertices:
-graph_a = [[3, 4], [2, 4]] # Complete the adjacency list for the Graph A
+graph_a = [[3, 4], [2, 4],[1,3,5],[0,2,5],[0,1,5],[2,3,4]] # Complete the adjacency list for the Graph A
 
 
 # Graph B: Directed graph
 # Vertices:
-graph_b = [[3, 4], [4]] # Complete the adjacency list for the Graph B
+graph_b = [[3, 4], [4],[1],[2],[5],[2,3]] # Complete the adjacency list for the Graph B
 
 
 def is_adjacent(graph, vert_a, vert_b):
-    #Complete the function
-    return False
+  x=0
+  l=len((graph[vert_a]))-1
+  while l>=0:
+    if (graph[vert_a])[l]==vert_b:
+      x=1
+    l=l-1  
+  if x==1:
+    return True
+  else:
+    return False    
+
+
 
 
 
