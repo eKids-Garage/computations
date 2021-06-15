44ade6292bfdb0dd6dcc7f44bb5f43a82ad8aba7
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2021-06-15 20:57:30 +0300

    Update rec_mul.py

--- recursion/rec_mul.py
+++ recursion/rec_mul.py
@@ -1,20 +1,25 @@
-# Реализуйте алгоритм рекурсивного умножения. 
-# Попробуйте реализовать его для чисел любого размера, 
-# но также можно ограничиться числами с размерностью 
-# степень двойки: 2, 4, 8, 16 и т.д.
+#задаем исходные переменные(количество цифр является степенью двойки)
+a = int(input("Input a(count of numerals should be the power of 2): "))
+b = int(input("Input b(count of numerals should be the power of 2): "))
+n = 0
 
-def rec_mul(x, y, n):
+#находим количество цифр в числе
+subNum = a
+while subNum != 0:
+  subNum = subNum//10
+  n += 1
+
+#функция в которой реализован алгоритм рекурсивного умножения
+def rec_mult(x, y, n):
   if n == 1:
-        print("n = 1: x = {0}, y = {1}".format(x, y))
-        return x*y
-    else: 
-        a = x // 10**(n//2)
-        b = x % 10**(n//2)
-        c = y // 10**(n//2)
-        d = y % 10**(n//2)
+    return x*y
+  else: 
+    a = x // 10**(n//2)
+    b = x % 10**(n//2)
+    c = y // 10**(n//2)
+    d = y % 10**(n//2)
 
-        print("n = {4}: a = {0}, b = {1}, c = {2}, d = {3}".format(a, b, c, d, n))
+    return 10**n * rec_mult(a, c, n//2) + 10**(n//2) * (rec_mult(a, d, n//2) + rec_mult(b, c, n//2)) + rec_mult(b, d, n//2)
 
-        return 10**n * rec_mult(a, c, n//2) + 10**(n//2) * (rec_mult(a, d, n//2) + rec_mult(b, c, n//2)) + rec_mult(b, d, n//2)
-
-print(rec_mult(9656, 8578, 4))
+#вывод конечного результата
+print("a * b = {0}".format(rec_mult(a, b, n)))
