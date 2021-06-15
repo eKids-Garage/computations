a504e5282e479f6c4a44a69d510e2535a5459acb
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2021-06-15 16:15:03 +0300

    Update adj_list.py

--- graph/adj_list.py
+++ graph/adj_list.py
@@ -1,24 +1,38 @@
-# Graph A: Non-directed graph
-# Vertices:
-graph_a = [[3, 4], [2, 4]] # Complete the adjacency list for the Graph A
+class graph:
+  def __init__(self, type, list):
+    self.type = type
+    self.list = list
 
+graph_a = graph("nonDirected", [[3, 5], [3, 2], [3, 0], [2, 5],
+ [2, 1], [1, 4], [0, 4], [4, 5]])
 
-# Graph B: Directed graph
-# Vertices:
-graph_b = [[3, 4], [4]] # Complete the adjacency list for the Graph B
+graph_b = graph("directed", [[5, 3], [3, 2], [0, 3], [5, 2], 
+  [2, 1], [1, 4], [0, 4], [4, 5]])
 
 
 def is_adjacent(graph, vert_a, vert_b):
-    #Complete the function
-    return False
-
-
-
+    if graph.type == "nonDirected":
+      i = 0
+      while i < len(graph.list):
+        if vert_a in graph.list[i] and vert_b in graph.list[i]:
+          return True
+        i += 1
+      return False 
+    else:
+      i = 0
+      while i < len(graph.list):
+        if graph.list[i][0] == vert_a and graph.list[i][1] == vert_b:
+          return True
+        i += 1
+      return False
+    
+print("Graph a: ")
 print(is_adjacent(graph_a, 5, 2))
 print(is_adjacent(graph_a, 0, 3))
 print(is_adjacent(graph_a, 3, 4))
 print(is_adjacent(graph_a, 2, 5))
 
+print("Graph b: ")
 print(is_adjacent(graph_b, 5, 2))
 print(is_adjacent(graph_b, 0, 3))
 print(is_adjacent(graph_b, 3, 4))
