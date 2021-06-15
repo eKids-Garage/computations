138c0f07a98fd711a0d17cdb82238c338f7804b9
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2021-06-15 17:07:41 +0300

    Update rational_num.py

--- graph/rational_num.py
+++ graph/rational_num.py
@@ -1,6 +1,17 @@
-# Напишите функцию, которая складывает два рациональных числа
-# Структуру данных для хранения чисел выберите и реализуйте сами
+a = []
+a.append(int(input("Input a - numerator:")))
+a.append(int(input("Input a - denominator:")))
 
+b = []
+b.append(int(input("Input b - numerator:")))
+b.append(int(input("Input b - denominator:")))
 
 def sum_rat(a, b):
+  return [a[0]*b[1] + b[0]*a[1], a[1]*b[1]]
+
+def sum_print(sum):
+  print("{0}/{1}".format(sum[0], sum[1]))
+
+sum = sum_rat(a, b)
+sum_print(sum)
   
