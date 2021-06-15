785ac23349141c1b961382a31c151aa74e7ecac5
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2021-06-15 15:24:50 +0300

    Update eratosthenes.py

--- recursion/eratosthenes.py
+++ recursion/eratosthenes.py
@@ -1,11 +1,38 @@
-# Написать программу, которая находит все простые числа в заданном 
-# интервале методом "Решето Эратосфена". 
-# start и finish - границы диапазона (включительно).
+def sieve(c, p):
+  global A
+  if c < len(A):
+    i = p-1
+    while i < len(A):
+      if (i+2) % p == 0:
+        A[i] = False
+      i += 1
+    
+    j = p - 1  
+    while j < len(A):
+      if A[j] == True:
+        p = j+2
+        break
+      j += 1
+    sieve(c+1, p)
 
-import math
+#Задаем верхнюю и нижнюю границы
+n = int(input("Input n: "))
+m = int(input("Input m: "))
 
-def sieve(start, finish):
-  primes = []
-  return primes
+p = 2
+A = []
+i = 0
+while i <= m-p:
+  A.append(True)
+  i += 1
 
-print(sieve(10, 20))
+
+
+c = 0
+sieve(c, p)
+
+#Выводим только подходящие простые числа
+while c < len(A):
+  if A[c] == True and (c+2)>=n:
+    print(c+2)
+  c += 1
